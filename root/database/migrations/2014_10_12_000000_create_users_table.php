27319eed6de3e8d38e86205a6f2f5db40d2a0d29
ShujiYamamoto <shuji.yamamoto@epkotsoftware.co.jp>
2023-06-23 17:15:49 +0900

    usersテーブル（マイグレーションファイル）

--- root/database/migrations/2014_10_12_000000_create_users_table.php
+++ root/database/migrations/2014_10_12_000000_create_users_table.php
@@ -12,13 +12,14 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
-            $table->timestamps();
+            $table->id()->comment('ID');
+            $table->boolean('admin')->comment('権限');
+            $table->char('username')->comment('ユーザー名');
+            $table->char('password')->comment('パスワード');
+            $table->char('mail_address')->comment('メールアドレス');
+            $table->timestamp('deleted_at')->comment('削除日時');
+            $table->timestamp('created_at')->comment('作成日時');
+            $table->timestamp('updated_at')->comment('更新日時');
         });
     }
 
