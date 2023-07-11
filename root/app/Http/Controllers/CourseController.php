197c7a07d84a7480d111402d3da5d55f85c9d02c
ShujiYamamoto <shuji.yamamoto@epkotsoftware.co.jp>
2023-07-11 17:46:38 +0900

    コースコントローラー

---
+++ root/app/Http/Controllers/CourseController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreCourseRequest;
+use App\Http\Requests\UpdateCourseRequest;
+use Illuminate\Http\Request;
+use App\Models\Course;
+
+class CourseController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $courses = Course::orderby('position')->get();
+
+        return view('admin.courses.index', compact('courses'));
+    }
+
+    public function sort(Request $request)
+    {
+        $positions = $request->input('positions');
+        foreach ($positions as $index => $id) {
+            Course::where('id', $id)->update(['position' => $index + 1]);
+        }
+
+        return response()->json(['success' => true]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        return view('admin.courses.add');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreCourseRequest $request)
+    {
+
+        Course::create([
+            'title'       => $request->title,
+            'introduction' => $request->introduction,
+            'remarks'      => $request->remarks,
+        ]);
+
+        return redirect()->route('course.index')->with('message', 'コースを登録しました');
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Course $course)
+    {
+        return view('admin.courses.edit', compact('course'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateCourseRequest $request, Course $course)
+    {
+        Course::find($course->id)->update([
+            'title'       => $request->title,
+            'introduction' => $request->introduction,
+            'remarks'      => $request->remarks,
+        ]);
+
+        return redirect()->route('course.index');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Course $course)
+    {
+        $course->delete();
+        return redirect()->route('course.index')->with('danger', $course->title . 'を削除しました');
+    }
+}
