f4b3da49bc8cd8ad2735416aa6727e3174e0714e
ShujiYamamoto <shuji.yamamoto@epkotsoftware.co.jp>
2023-07-14 13:15:51 +0900

    【変更】deleted_at => null, hh:mm:ssの重複部分を修正

--- root/database/seeders/UserSeeder.php
+++ root/database/seeders/UserSeeder.php
@@ -23,9 +23,9 @@ class UserSeeder extends Seeder
                         'username' => sprintf('test_%02d', $sequence->index + 1),
                         'password' => Hash::make('test'),
                         'mail_address' => sprintf('test_%02d@test', $sequence->index + 1),
-                        'deleted_at' => '2023-06-30 00:50:00',
-                        'created_at' => '2023-06-01 01:22:34',
-                        'updated_at' => '2023-06-22 12:12:22',
+                        'deleted_at' => null,
+                        'created_at' => '2023-06-01 01:23:45',
+                        'updated_at' => '2023-06-30 21:58:59',
                     ];
                 })
                 ->create();
