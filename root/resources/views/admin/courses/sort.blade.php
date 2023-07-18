fd58764cac32f34417bd38b7bc5161df6e19224e
ShujiYamamoto <shuji.yamamoto@epkotsoftware.co.jp>
2023-07-18 17:51:19 +0900

    jQueryからJavaScriptに修正

--- root/resources/views/admin/courses/sort.blade.php
+++ root/resources/views/admin/courses/sort.blade.php
@@ -1,34 +1,70 @@
-{{-- JQuery --}}
-<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-<script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
-<!-- JavaScript Bundle with Popper -->
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
-<script>
-    $(function() {
-        $("#sortable tbody").sortable({
-            update: function(event, ui) {
-                var positions = [];
-                $("#sortable tbody tr").each(function(index) {
-                    positions.push($(this).data("id"));
-                });
+<!-- JavaScript Bundle with Popper ver.5.2.3 -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
 
-                $.ajax({
-                    url: "{{ route('course.sort') }}",
-                    method: "POST",
-                    data: {
-                        positions: positions
-                    },
-                    headers: {
-                        'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
-                    },
-                    success: function() {
-                        console.log("成功")
-                    },
-                    error: function(xhr, status, error) {
-                        console.error(error);
-                    }
-                });
+<script>
+let draggedRow = null;
+
+function handleDragStart(event) {
+    draggedRow = event.target;
+    event.dataTransfer.effectAllowed = 'move';
+}
+
+function handleDragOver(event) {
+    event.preventDefault();
+    event.dataTransfer.dropEffect = 'move';
+
+    const targetRow = event.target.closest('tr');
+    if (targetRow && targetRow !== draggedRow) {
+        const tbody = targetRow.parentNode;
+        const rect = targetRow.getBoundingClientRect();
+        const next = (event.clientY - rect.top) > (rect.bottom - event.clientY);
+
+        if (next) {
+            tbody.insertBefore(draggedRow, targetRow.nextSibling);
+        } else {
+            tbody.insertBefore(draggedRow, targetRow);
+        }
+    }
+}
+
+function handleDragEnd() {
+    draggedRow = null;
+}
+
+// 非同期処理
+async function saveSortOrder() {
+    const rows = document.querySelectorAll("#sortable tbody tr");
+    const positions = Array.from(rows).map(row => row.dataset.id);
+
+    try {
+        const response = await fetch("{{ route('course.sort') }}", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+                "X-CSRF-TOKEN": document.querySelector('meta[name="csrf-token"]').getAttribute('content')
             },
-        }).disableSelection();
+            body: JSON.stringify({ positions: positions })
+        });
+
+        const data = await response.json();
+        console.log(data.success);
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+
+function addDragHandlers() {
+    const rows = document.querySelectorAll("#sortable tbody tr");
+    rows.forEach(row => {
+        row.addEventListener('dragstart', handleDragStart);
+        row.addEventListener('dragover', handleDragOver);
+        row.addEventListener('dragend', handleDragEnd);
+        row.setAttribute('draggable', true);
     });
-</script>
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+    addDragHandlers();
+    document.querySelector("#sortable tbody").addEventListener('dragend', saveSortOrder);
+});</script>
