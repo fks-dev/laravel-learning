b8e0841b3530c661bf3546a4cad5c5e1f71d6d68
ShujiYamamoto <shuji.yamamoto@epkotsoftware.co.jp>
2023-07-19 16:57:05 +0900

    【変更】sort.blade.php 並び替えにSortableJS使用、並べ替え完了後ボタンで更新

--- root/resources/views/admin/courses/sort.blade.php
+++ root/resources/views/admin/courses/sort.blade.php
@@ -1,70 +1,64 @@
 <!-- JavaScript Bundle with Popper ver.5.2.3 -->
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
+<!-- SortableJS -->
+<script defer src="https://cdn.jsdelivr.net/npm/sortablejs@1.15.0/Sortable.min.js" integrity="sha384-eeLEhtwdMwD3X9y+8P3Cn7Idl/M+w8H4uZqkgD/2eJVkWIN1yKzEj6XegJ9dL3q0" crossorigin="anonymous"></script>
 
 <script>
-let draggedRow = null;
 
-function handleDragStart(event) {
-    draggedRow = event.target;
-    event.dataTransfer.effectAllowed = 'move';
-}
+document.addEventListener("DOMContentLoaded", (event) => {
 
-function handleDragOver(event) {
-    event.preventDefault();
-    event.dataTransfer.dropEffect = 'move';
+    // 並び替えの処理
+    new Sortable(document.getElementById("tableBody"), {
+        animation: 150,
+        onEnd: function (event) {
+            const saveBtn = document.getElementById("saveBtn");
+            const backBtn = document.getElementById("backBtn");
+            saveBtn.disabled = false;
+            backBtn.disabled = false;
+        }
+    });
 
-    const targetRow = event.target.closest('tr');
-    if (targetRow && targetRow !== draggedRow) {
-        const tbody = targetRow.parentNode;
-        const rect = targetRow.getBoundingClientRect();
-        const next = (event.clientY - rect.top) > (rect.bottom - event.clientY);
+    // 元に戻すボタンを押した時の処理（リロード）
+    const back = document.getElementById("backBtn");
+    back.addEventListener("click", function() {
+        back.disabled = false;
+        location.reload();
+    })
 
-        if (next) {
-            tbody.insertBefore(draggedRow, targetRow.nextSibling);
-        } else {
-            tbody.insertBefore(draggedRow, targetRow);
+    // 変更確定ボタンを押した時の処理
+    const save = document.getElementById("saveBtn");
+    save.addEventListener("click", function() {
+        save.disabled = true;
+        back.disabled = true;
+        saveSortOrder();
+    });
+
+    // 非同期処理
+    async function saveSortOrder() {
+        const rows = document.querySelectorAll("#sortable tbody tr");
+        const positions = Array.from(rows).map(row => row.dataset.id);
+
+        try {
+            const response = await fetch("{{ route('course.sort') }}", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                    "X-CSRF-TOKEN": document.querySelector('meta[name="csrf-token"]').getAttribute('content')
+                },
+                body: JSON.stringify({ positions: positions })
+            });
+
+            const data = await response.json();
+            console.log(data.message);
+
+            // 並び替えが完了したことを表示
+            const message = document.getElementById('message');
+            const messageClass = message.classList.add("alert", "alert-success");
+            message.innerText = data.message;
+        } catch (error) {
+            console.error(error);
         }
     }
-}
+});
 
-function handleDragEnd() {
-    draggedRow = null;
-}
-
-// 非同期処理
-async function saveSortOrder() {
-    const rows = document.querySelectorAll("#sortable tbody tr");
-    const positions = Array.from(rows).map(row => row.dataset.id);
-
-    try {
-        const response = await fetch("{{ route('course.sort') }}", {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json",
-                "X-CSRF-TOKEN": document.querySelector('meta[name="csrf-token"]').getAttribute('content')
-            },
-            body: JSON.stringify({ positions: positions })
-        });
-
-        const data = await response.json();
-        console.log(data.success);
-    } catch (error) {
-        console.error(error);
-    }
-}
-
-
-function addDragHandlers() {
-    const rows = document.querySelectorAll("#sortable tbody tr");
-    rows.forEach(row => {
-        row.addEventListener('dragstart', handleDragStart);
-        row.addEventListener('dragover', handleDragOver);
-        row.addEventListener('dragend', handleDragEnd);
-        row.setAttribute('draggable', true);
-    });
-}
-
-document.addEventListener('DOMContentLoaded', () => {
-    addDragHandlers();
-    document.querySelector("#sortable tbody").addEventListener('dragend', saveSortOrder);
-});</script>
+</script>
