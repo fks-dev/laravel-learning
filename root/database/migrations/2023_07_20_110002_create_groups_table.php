5943d88f4aadf5e4a30c71d3fef049a89b1be004
ShujiYamamoto <shuji.yamamoto@epkotsoftware.co.jp>
2023-07-20 13:41:02 +0900

    groupテーブル

---
+++ root/database/migrations/2023_07_20_110002_create_groups_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('groups', function (Blueprint $table) {
+            $table->id()->comment('ID');
+            $table->char('group_name')->comment('グループ名');
+            $table->text('remarks')->nullable()->comment('備考');
+            $table->timestamp('deleted_at')->nullable()->comment('削除日時');
+            $table->timestamp('created_at')->comment('作成日時');
+            $table->timestamp('updated_at')->comment('更新日時');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('groups');
+    }
+};
