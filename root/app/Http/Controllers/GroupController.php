de2db2cafcf605057754278106f9bb743396eb75
ShujiYamamoto <shuji.yamamoto@epkotsoftware.co.jp>
2023-07-20 13:34:56 +0900

    GroupController

---
+++ root/app/Http/Controllers/GroupController.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreGroupRequest;
+use App\Http\Requests\UpdateGroupRequest;
+use App\Models\Group;
+
+class GroupController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $groups = Group::all();
+        return view('admin.groups.index', compact('groups'));
+    }
+
+    // 並び替え
+    public function sort()
+    {
+
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        return view('admin.groups.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreGroupRequest $request)
+    {
+        Group::create([
+            'group_name' => $request->group_name,
+            'remarks'    => $request->remarks,
+        ]);
+
+        return redirect()->route('group.index')->with('message', 'グループを登録しました');
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Group $group)
+    {
+        return view('admin.groups.edit', compact('group'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateGroupRequest $request, Group $group)
+    {
+        Group::find($group->id)->update([
+            'group_name' => $request->group_name,
+            'remarks'    => $request->remarks,
+        ]);
+
+        return redirect()->route('group.index')->with('message', 'グループを編集しました');
+
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Group $group)
+    {
+        $group->delete();
+        return redirect()->route('group.index')->with('danger', $group->group_name . 'を削除しました');
+    }
+}
