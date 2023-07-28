5f7ad9f97f56128915bb6ca471889afea07fe7d7
ShujiYamamoto <shuji.yamamoto@epkotsoftware.co.jp>
2023-07-28 15:25:45 +0900

    【修正】ログイン画面（head部分共通化）

--- root/resources/views/admin/login.blade.php
+++ root/resources/views/admin/login.blade.php
@@ -1,28 +1,29 @@
 <!DOCTYPE html>
 <html lang="ja">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    @include('admin.head')
     <title>管理者ログイン画面</title>
 </head>
 <body>
-    <h2>管理者ログイン画面</h2>
-    <form action="{{ route('admin.login.store') }}" method="POST">
-        @csrf
-        <div>
-            <label for="username">ユーザー名：</label>
-            <input type="text" name="username" id="username" required />
-        </div>
-        <div>
-            <label for="password">パスワード：</label>
-            <input type="password" name="password" id="password" required />
-        </div>
-        <div>
-            @error('failed')
-                <p style="color:red">{{ $message }}</p>
-            @enderror
-            <button type="submit">ログイン</button>
-        </div>
-    </form>
+    <div class="mt-5 container">
+        <h2>管理者ログイン画面</h2>
+        <form action="{{ route('admin.login.store') }}" method="POST">
+            @csrf
+            <div>
+                <label for="username">ユーザー名：</label>
+                <input type="text" name="username" id="username" required />
+            </div>
+            <div>
+                <label for="password">パスワード：</label>
+                <input type="password" name="password" id="password" required />
+            </div>
+            <div>
+                @error('failed')
+                    <p style="color:red">{{ $message }}</p>
+                @enderror
+                <button type="submit">ログイン</button>
+            </div>
+        </form>
+    </div>
+</body>
 </html>
