7923560a1b54a50216976853545840d856062e0e
ShujiYamamoto <shuji.yamamoto@epkotsoftware.co.jp>
2023-07-28 15:27:33 +0900

    管理者追加画面

---
+++ root/resources/views/admin/create.blade.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    @include('admin.head')
+    <title>管理者登録</title>
+</head>
+<body>
+    <div class="mt-3 container">
+        <a href="{{ route('admin.index') }}">&lt;&lt;戻る</a>
+        <div class="border">
+            <div class="p-2 bg-secondary text-white">新規管理者登録</div>
+
+            <form action="{{ route('admin.store') }}" method="post">
+                @csrf
+                <div class="mx-5 px-5">
+
+                    <div class="row m-3">
+                        <label class="col-sm-2 col-form-label fw-bold" for="username">管理者ID
+                            <span class="text-danger fw-bold">＊</span>
+                        </label>
+                        <div class="col-sm-10">
+                            <input class="form-control" type="text" name="username" id="username" required>
+                        </div>
+                    </div>
+
+                    <div class="row m-3">
+                        <label class="col-sm-2 col-form-label fw-bold" for="password">パスワード
+                            <span class="text-danger fw-bold">＊</span>
+                        </label>
+                        <div class="col-sm-10">
+                            <input class="form-control" type="password" name="password" id="password" required>
+                        </div>
+                    </div>
+
+                    <div class="row m-3">
+                        <label class="col-sm-2 col-form-label fw-bold" for="mail_address">メールアドレス
+                            <span class="text-danger fw-bold">＊</span>
+                        </label>
+                        <div class="col-sm-10">
+                            <input class="form-control" type="email" name="mail_address" id="mail_address" required>
+                        </div>
+                    </div>
+
+                    <div class="row m-3">
+                        <label class="col-sm-2 col-form-label fw-bold" for="group">所属グループ</label>
+                        <div class="col-sm-10">
+                            <input class="form-control" type="email" name="group" id="group">
+
+                            {{-- 登録ボタン --}}
+                            <input class="form-contorl btn btn-primary mt-3" type="submit" value="登録">
+                        </div>
+                    </div>
+
+                </div>
+            </form>
+        </div>
+    </div>
+</body>
+</html>
