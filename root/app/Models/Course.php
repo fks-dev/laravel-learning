758ef788ddea1e5e752cc9cdc60d594f59427369
ShujiYamamoto <shuji.yamamoto@epkotsoftware.co.jp>
2023-08-03 13:35:54 +0900

    【変更】Coureseモデル（コースが削除された際に中間テーブルのデータが削除されるように追記）

--- root/app/Models/Course.php
+++ root/app/Models/Course.php
@@ -23,5 +23,25 @@ class Course extends Model
         static::creating(function ($model) {
             $model->position = Course::max('position') + 1;
         });
+
+        // コースが削除された時に、IDに紐づく中間テーブルの値も削除される
+        parent::boot();
+        static::deleting(function ($course) {
+            $course->UsersCoursesTable()->delete();
+        });
+
     }
+
+    public function Users()
+    {
+        return $this->belongsToMany(User::class, 'users_courses', 'course_id', 'user_id');
+    }
+
+    // users_coursesテーブルとのリレーション
+    public function UsersCoursesTable()
+    {
+        return $this->hasMany(UsersCourse::class, 'course_id', 'id');
+    }
+
+
 }
