d0091fdcf1848481f5ac4f351a1d78439ac6b527
ShujiYamamoto <shuji.yamamoto@epkotsoftware.co.jp>
2023-08-03 13:41:45 +0900

    中間テーブルマイグレーション（userテーブル、courseテーブル）

---
+++ root/database/migrations/2023_07_31_173409_create_users_courses_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('users_courses', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('user_id');
+            $table->unsignedBigInteger('course_id');
+            $table->timestamp('deleted_at')->nullable()->comment('削除日時');
+            $table->timestamp('updated_at')->default(now())->comment('更新日時');
+            $table->timestamp('created_at')->default(now())->comment('作成日時');
+
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->foreign('course_id')->references('id')->on('courses')->onDelete('cascade');
+
+            $table->unique(['user_id', 'course_id']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('users_courses');
+    }
+};
