19cb97131e569adae6bd3641c45923bc6df154cb
ShujiYamamoto <shuji.yamamoto@epkotsoftware.co.jp>
2023-08-03 13:56:43 +0900

    ルーティング

--- root/routes/web.php
+++ root/routes/web.php
@@ -2,7 +2,7 @@
 
 use App\Http\Controllers\CourseController;
 use App\Http\Controllers\AdminController;
-use App\Http\Controllers\AdminLogController;
+use App\Http\Controllers\UserMgmtController;
 use App\Http\Controllers\AdminLoginController;
 use App\Http\Controllers\UserLoginController;
 use Illuminate\Support\Facades\Route;
@@ -22,17 +22,21 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-// 管理ログイン画面
-Route::get('/admin/login', [AdminLoginController::class, 'create'])->name('admin.login');
-// 管理ログイン
-Route::post('/admin/login', [AdminLoginController::class, 'store'])->name('admin.login.store');
-// 管理ログアウト
-Route::delete('/admin/login', [AdminLoginController::class, 'destroy'])->name('admin.login.destroy');
+Route::prefix('admin')->name('admin.login')->controller(AdminLoginController::class)->group(function () {
+    // 管理ログイン画面
+    Route::get('', 'create')->name('');
+    // 管理ログイン
+    Route::post('', 'store')->name('.store');
+    // 管理ログアウト
+    Route::delete('', 'destroy')->name('.destroy');
+});
+
 
 // 管理ログイン後のみアクセス可
 Route::middleware('auth:admin')->group(function () {
     // コース画面
     Route::prefix('admin')->group(function () {
+
         Route::prefix('courses')->name('course')->controller(CourseController::class)->group(function () {
             Route::get('', 'index')->name('.index');
             Route::post('sort', 'sort')->name('.sort');
@@ -42,23 +46,38 @@ Route::middleware('auth:admin')->group(function () {
             Route::patch('{course}', 'update')->name('.update');
             Route::delete('{course}', 'destroy')->name('.destroy');
         });
-    });
 
-    // 管理者一覧画面
-    Route::prefix('admin')->name('admin')->controller(AdminController::class)->group(function() {
-        Route::get('', 'index')->name('.index');
-        Route::post('search', 'search')->name('.search');
-        Route::post('sort', 'sort')->name('.sort');
-        Route::get('create', 'create')->name('.create');
-        Route::post('', 'store')->name('.store');
-        Route::get('{admin}/edit', 'edit')->name('.edit');
-        Route::patch('{admin}', 'update')->name('.update');
-        Route::delete('{admin}', 'destroy')->name('.destroy');
-        // CSVエクスポート
-        Route::post('csv', 'csv')->name('.csv');
-        // CSVインポート
-        Route::get('import', 'import')->name('.import');
-        Route::post('import', 'importStore')->name('.import.store');
+        // 管理者一覧画面
+        Route::prefix('adminMgmt')->name('adminMgmt')->controller(AdminController::class)->group(function() {
+            Route::get('', 'index')->name('.index');
+            Route::post('search', 'search')->name('.search');
+            Route::get('create', 'create')->name('.create');
+            Route::post('', 'store')->name('.store');
+            Route::get('{admin}/edit', 'edit')->name('.edit');
+            Route::patch('{admin}', 'update')->name('.update');
+            Route::delete('{admin}', 'destroy')->name('.destroy');
+            // CSVエクスポート
+            Route::post('csv', 'csv')->name('.csv');
+            // CSVインポート
+            Route::get('import', 'import')->name('.import');
+            Route::post('import', 'importStore')->name('.import.store');
+        });
+
+        // ユーザー　一覧画面
+        Route::prefix('userMgmt')->name('userMgmt')->controller(UserMgmtController::class)->group(function() {
+            Route::get('', 'index')->name('.index');
+            Route::post('search', 'search')->name('.search');
+            Route::get('create', 'create')->name('.create');
+            Route::post('', 'store')->name('.store');
+            Route::get('{user}/edit', 'edit')->name('.edit');
+            Route::patch('{user}', 'update')->name('.update');
+            Route::delete('{user}', 'destroy')->name('.destroy');
+            // CSVエクスポート
+            Route::post('csv', 'csv')->name('.csv');
+            // CSVインポート
+            Route::get('import', 'import')->name('.import');
+            Route::post('import', 'importStore')->name('.import.store');
+        });
     });
 });
 
