7cd1e8408283e91d05cd739a79b0223f2ef0010d
ShujiYamamoto <shuji.yamamoto@epkotsoftware.co.jp>
2023-08-03 13:28:09 +0900

    【変更】AdminLoginコントローラーにログイン日時を記録するように追記

--- root/app/Http/Controllers/AdminLoginController.php
+++ root/app/Http/Controllers/AdminLoginController.php
@@ -7,7 +7,7 @@ use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\View\View;
-use App\Models\LoginLog;
+use App\Models\AdminLog;
 
 class AdminLoginController extends Controller
 {
@@ -30,13 +30,20 @@ class AdminLoginController extends Controller
         if (Auth::guard('admin')->check()) {
             $user = Auth::guard('admin')->user();
 
-            $loginLog = new LoginLog();
+            // 過去ログを確認
+            $loginLog = AdminLog::where('admin_id', $user->id)->first();
 
-            $loginLog->admin_id = $user->id;
-            $loginLog->login_at = now();
-            $loginLog->save();
+            if ($loginLog) {
+                $loginLog->updated_at = now();
+                $loginLog->save();
+            } else {
+                $newLoginLog = new AdminLog();
+                $newLoginLog->admin_id = $user->id;
+                $newLoginLog->updated_at = now();
+                $newLoginLog->save();
+            }
         }
-        return redirect()->intended(route('admin.index'));
+        return redirect()->intended(route('adminMgmt.index'));
 
     }
 
