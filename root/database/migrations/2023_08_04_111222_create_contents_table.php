2f0b10942247eb68c298bbad5ba938fdb6bfa34b
ShujiYamamoto <shuji.yamamoto@epkotsoftware.co.jp>
2023-08-18 15:36:57 +0900

    コンテンツテーブル（マイグレーションファイル）

---
+++ root/database/migrations/2023_08_04_111222_create_contents_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('contents', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('course_id')->comment('コースID');
+            $table->unsignedBigInteger('admin_id')->comment('管理者ID');
+            $table->char('title')->comment('タイトル');
+            $table->char('kind')->comment('コンテンツ種別');
+            $table->text('text')->nullable()->comment('リッチテキスト');
+            $table->char('url')->nullable()->comment('URL');
+            $table->string('movie')->nullable()->comment('動画');
+            $table->string('file')->nullable()->comment('配布資料');
+            $table->integer('testTime')->nullable()->comment('テスト制限時間');
+            $table->integer('testPer')->nullable()->comment('合格得点率');
+            $table->integer('testVol')->nullable()->comment('出題数');
+            $table->text('remarks')->nullable()->comment('備考');
+            $table->boolean('public')->comment('公開・非公開');
+            $table->unsignedInteger('position')->default(0)->comment('ソート番号');
+            $table->timestamp('deleted_at')->nullable()->comment('削除日時');
+            $table->timestamp('created_at')->comment('削除日時');
+            $table->timestamp('updated_at')->comment('更新日時');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('contents');
+    }
+};
