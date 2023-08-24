17361fff370c2f852db436030996806b372cdce7
ShujiYamamoto <shuji.yamamoto@epkotsoftware.co.jp>
2023-08-24 11:27:20 +0900

    メッセージ詳細画面（管理者側）

---
+++ root/resources/views/admin/messages/show.blade.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    @include('admin.courses.head')
+    <title>メッセージ内容</title>
+</head>
+<body>
+    <div class="mt-5 container">
+        <div class="mb-2">
+            <a class="btn btn-secondary" href="{{ isset($admin) ? route('admin.message.sent') : route('admin.message.index') }}">戻る</a>
+        </div>
+        <div class="mb-2">
+            @if ( empty($admin) )
+                <a class="btn btn-success" href="{{ route('admin.message.reply', $message) }}">返信</a>
+            @endif
+        </div>
+
+        <div>
+            <p>件名：{{ $message->title }}</p>
+        </div>
+        <div>
+            <p>@if (isset($admin)) 宛先 @else 差出人 @endif：
+                @foreach ($users as $user)
+                    {{ $message->user_id == $user->id ? $user->username : '' }}
+                @endforeach
+            </p>
+        </div>
+        <div>
+            <p>本文</p>
+            {!! nl2br(htmlspecialchars($message->text)) !!}
+        </div>
+    </div>
+</body>
+</html>
