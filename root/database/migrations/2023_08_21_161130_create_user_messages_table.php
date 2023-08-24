28eba3142fe84791cb116d7ddd98fec8061009f3
ShujiYamamoto <shuji.yamamoto@epkotsoftware.co.jp>
2023-08-24 11:14:42 +0900

    マイグレーション（ユーザーテーブル）

---
+++ root/database/migrations/2023_08_21_161130_create_user_messages_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('user_messages', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('admin_id');
+            $table->unsignedBigInteger('user_id');
+            $table->string('title')->comment('件名');
+            $table->text('text')->nullable()->comment('本文');
+            $table->text('draft')->nullable()->comment('下書き');
+            $table->timestamp('deleted_at')->nullable()->comment('削除日時');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('user_messages');
+    }
+};
