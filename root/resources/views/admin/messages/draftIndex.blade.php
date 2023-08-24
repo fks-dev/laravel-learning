47bda8ab9dfd6b9a4171ccc2b104a8cfe94f3db7
ShujiYamamoto <shuji.yamamoto@epkotsoftware.co.jp>
2023-08-24 11:24:29 +0900

    メッセージ下書き一覧画面（管理者側）

---
+++ root/resources/views/admin/messages/draftIndex.blade.php
@@ -0,0 +1,73 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    @include('admin.courses.head')
+    <title>メッセージ一覧</title>
+</head>
+
+<body>
+<div class="mt-5 container">
+    <div class="d-flex justify-content-between">
+        <h2 class="col">下書き一覧</h2>
+        <div class="col-auto">
+            <a class="btn btn-info" href="{{ route('admin.message.index')}}">受信</a>
+            <a class="btn btn-secondary" href="{{ route('admin.message.sent')}}">送信済み</a>
+            <a class="btn btn-primary" href="{{ route('admin.message.create')}}">&plus;追加</a>
+        </div>
+    </div>
+{{-- 登録・削除　メッセージ --}}
+    <div id="message"></div>
+    @if (session('message'))
+        <div class="alert alert-success">
+        {{ session('message') }}
+        </div>
+    @elseif (session('danger'))
+    <div class="alert alert-danger">
+        {{ session('danger') }}
+        </div>
+    @endif
+
+    <table class="table table-striped" id="sortable">
+        <thead>
+            <tr>
+                <th class="col-3">件名</th>
+                <th class="col-2">宛先</th>
+                <th class="col-5">本文</th>
+                <th class="col-2 text-center">保存日時</th>
+                <th class="col-2 text-center">Actions</th>
+                </tr>
+        </thead>
+
+        <tbody id="tableBody">
+
+            @foreach ($messages as $message)
+                <tr data-id="{{ $message->id }}">
+                    <td class="align-middle">
+                        <a href="{{ route('admin.message.edit', $message) }}">{{ Str::limit($message->title, $limit = 28, $end = '...') }}</a>
+                    </td>
+                    <td class="align-middle">
+                        @foreach ($users as $user)
+                            {{ $message->user_id == $user->id ? $user->username : ''}}
+                        @endforeach
+                    </td>
+                    <td class="align-middle">{{ Str::limit($message->draft, $limit = 50, $end = '...') }}</td>
+                    <td class="align-middle text-center">{{ $message->created_at }}</td>
+
+                    <td class="text-center">
+                        <form action="{{ route('admin.message.destroy', $message) }}" method="post" class="d-inline">
+                            @csrf
+                            @method('delete')
+                            <input class="btn btn-danger" type="submit" value="削除"
+                            onClick="return confirm('本当に削除しますか？');">
+                        </form>
+
+                    </td>
+                </tr>
+            @endforeach
+
+        </tbody>
+
+    </table>
+</div>
+</body>
+</html>
