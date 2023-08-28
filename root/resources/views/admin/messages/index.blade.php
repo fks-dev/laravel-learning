d4cfc37461ada20a6111f6317ead196829288f4e
ShujiYamamoto <shuji.yamamoto@epkotsoftware.co.jp>
2023-08-28 13:49:36 +0900

    【修正】メッセージ一覧画面（不要部分削除、ボタン追加）

--- root/resources/views/admin/messages/index.blade.php
+++ root/resources/views/admin/messages/index.blade.php
@@ -10,24 +10,29 @@
     <div class="d-flex justify-content-between">
         <h2 class="col">受信一覧</h2>
         <div class="col-auto">
+            <a class="btn btn-primary" href="{{ route('admin.message.create')}}">&plus;追加</a>
             <a class="btn btn-success" href="{{ route('admin.message.draft')}}">下書き</a>
             <a class="btn btn-secondary" href="{{ route('admin.message.sent')}}">送信済み</a>
-            <a class="btn btn-primary" href="{{ route('admin.message.create')}}">&plus;追加</a>
+            <a class="btn btn-danger" href="{{ route('admin.message.dust')}}">ゴミ箱</a>
         </div>
     </div>
-{{-- 登録・削除　メッセージ --}}
-    <div id="message"></div>
+
+    {{-- 登録・削除　メッセージ --}}
     @if (session('message'))
         <div class="alert alert-success">
-        {{ session('message') }}
+            {{ session('message') }}
         </div>
     @elseif (session('danger'))
-    <div class="alert alert-danger">
-        {{ session('danger') }}
+        <div class="alert alert-danger">
+            {{ session('danger') }}
+        </div>
+    @elseif (session('success'))
+        <div class="alert alert-success">
+            {{ session('success') }}
         </div>
     @endif
 
-    <table class="table table-striped" id="sortable">
+    <table class="table table-striped">
         <thead>
             <tr>
                 <th class="col-3">件名</th>
@@ -38,12 +43,14 @@
                 </tr>
         </thead>
 
-        <tbody id="tableBody">
+        <tbody>
 
             @foreach ($messages as $message)
                 <tr data-id="{{ $message->id }}">
                     <td class="align-middle">
-                        <a href="{{ route('admin.message.show', $message) }}">{{ Str::limit($message->title, $limit = 28, $end = '...') }}</a>
+                        <a href="{{ route('admin.message.show', $message) }}">
+                            {{ Str::limit($message->title, $limit = 28, $end = '...') }}
+                        </a>
                     </td>
                     <td class="align-middle">
                         @foreach ($users as $user)
@@ -54,19 +61,17 @@
                     <td class="align-middle text-center">{{ $message->created_at }}</td>
 
                     <td class="text-center">
-                        <form action="{{ route('admin.message.destroy', $message) }}" method="post" class="d-inline">
+                        <form action="{{ route('admin.message.hidden', $message) }}" method="post" class="d-inline">
                             @csrf
-                            @method('delete')
-                            <input class="btn btn-danger" type="submit" value="削除"
+                            <input class="btn btn-danger" name="action" type="submit" value="削除"
                             onClick="return confirm('本当に削除しますか？');">
                         </form>
-
                     </td>
+
                 </tr>
             @endforeach
 
         </tbody>
-
     </table>
 </div>
 </body>
