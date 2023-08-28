4744228d43cf5d83050f593b3e70309cdfe171cf
ShujiYamamoto <shuji.yamamoto@epkotsoftware.co.jp>
2023-08-28 13:49:49 +0900

    【修正】送信済み一覧画面（不要部分削除、ボタン追加）

--- root/resources/views/admin/messages/sentIndex.blade.php
+++ root/resources/views/admin/messages/sentIndex.blade.php
@@ -10,24 +10,24 @@
     <div class="d-flex justify-content-between">
         <h2 class="col">送信済み一覧</h2>
         <div class="col-auto">
+            <a class="btn btn-primary" href="{{ route('admin.message.create')}}">&plus;追加</a>
             <a class="btn btn-info" href="{{ route('admin.message.index')}}">受信</a>
             <a class="btn btn-success" href="{{ route('admin.message.draft')}}">下書き</a>
-            <a class="btn btn-primary" href="{{ route('admin.message.create')}}">&plus;追加</a>
+            <a class="btn btn-danger" href="{{ route('admin.message.dust')}}">ゴミ箱</a>
         </div>
     </div>
 {{-- 登録・削除　メッセージ --}}
-    <div id="message"></div>
     @if (session('message'))
         <div class="alert alert-success">
-        {{ session('message') }}
+            {{ session('message') }}
         </div>
     @elseif (session('danger'))
-    <div class="alert alert-danger">
-        {{ session('danger') }}
+        <div class="alert alert-danger">
+            {{ session('danger') }}
         </div>
     @endif
 
-    <table class="table table-striped" id="sortable">
+    <table class="table table-striped">
         <thead>
             <tr>
                 <th class="col-3">件名</th>
@@ -38,12 +38,14 @@
                 </tr>
         </thead>
 
-        <tbody id="tableBody">
+        <tbody>
 
             @foreach ($messages as $message)
                 <tr data-id="{{ $message->id }}">
                     <td class="align-middle">
-                        <a href="{{ route('admin.message.sent.show', $message) }}">{{ Str::limit($message->title, $limit = 28, $end = '...') }}</a>
+                        <a href="{{ route('admin.message.sent.show', $message) }}">
+                            {{ Str::limit($message->title, $limit = 28, $end = '...') }}
+                        </a>
                     </td>
                     <td class="align-middle">
                         @foreach ($users as $user)
@@ -60,13 +62,12 @@
                             <input class="btn btn-danger" type="submit" value="削除"
                             onClick="return confirm('本当に削除しますか？');">
                         </form>
-
                     </td>
+
                 </tr>
             @endforeach
 
         </tbody>
-
     </table>
 </div>
 </body>
