d6b211dbd2387a8eef07e230d6a4fc3883636aab
ShujiYamamoto <shuji.yamamoto@epkotsoftware.co.jp>
2023-08-28 13:45:33 +0900

    AdminMessage Seeder,Factory

---
+++ root/database/seeders/AdminMessageSeeder.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\AdminMessage;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Str;
+
+
+class AdminMessageSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        if(app()->isLocal()) {
+            AdminMessage::factory()
+                ->count(10)
+                ->sequence(function($sequence) {
+                    return [
+                        'admin_id' => 1,
+                        'user_id' => 1,
+                        'title' => sprintf('メッセージ%d', $sequence->index + 1),
+                        'text' => Str::random(20),
+                        'draft' => null,
+                        'hidden' => 0,
+                        'created_at' => now(),
+                        'updated_at' => now(),
+                    ];
+                })->create();
+        }
+
+    }
+}
