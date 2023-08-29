19f2306afdb57c904037fb3d4c7d8dd84d0da40e
ShujiYamamoto <shuji.yamamoto@epkotsoftware.co.jp>
2023-08-29 14:11:15 +0900

    【変更】index レスポンシブに対応

--- root/resources/views/admin/contents/index.blade.php
+++ root/resources/views/admin/contents/index.blade.php
@@ -40,8 +40,8 @@
         <table class="table table-striped" id="sortable">
             <thead>
                 <tr>
-                    <th class="col-3">コンテンツ名</th>
-                    <th class="col-2 text-center">コンテンツ種別</th>
+                    <th class="col-2">コンテンツ名</th>
+                    <th class="col-1 text-center">コンテンツ種別</th>
                     <th class="col-1 text-center">ステータス</th>
                     <th class="col-2 text-center">作成日時</th>
                     <th class="col-2 text-center">更新日時</th>
@@ -55,20 +55,20 @@
                         <td class="align-middle">
                             <a href="{{ route('content.show', $content->id) }}">{{ $content->title }}</a>
                         </td>
-                        <td class="align-middle text-center">{{ $content->kind }}</td>
-                        <td class="align-middle text-center">{{ $content->public == 1 ? '公開' : '非公開' }}</td>
+                        <td class="align-middle text-center">@include('admin.contents.contentType')</td>
+                        <td class="align-middle text-center">{{ $content->is_public == 1 ? '公開' : '非公開' }}</td>
                         <td class="align-middle text-center">{{ $content->created_at }}</td>
                         <td class="align-middle text-center">{{ $content->updated_at }}</td>
 
                         <td class="text-center">
                             <a class="btn btn-success" href="{{ route('content.edit', $content->id) }}">編集</a>
 
-                            <form action="{{ route('content.duplicate', $content) }}" method="post" class="d-inline">
+                            <form action="{{ route('content.duplicate', $content) }}" method="post" class="action">
                                 @csrf
-                                <input class="btn btn-info text-white" type="submit" value="複製">
+                                <input class="btn btn-info text-white action" type="submit" value="複製">
                             </form>
 
-                            <form action="{{ route('content.destroy', $content) }}" method="post" class="d-inline">
+                            <form action="{{ route('content.destroy', $content) }}" method="post" class="action">
                                 @csrf
                                 @method('delete')
                                 <input class="btn btn-danger" type="submit" value="削除"
