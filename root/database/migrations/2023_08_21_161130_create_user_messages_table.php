85da81fd2a66dd766ce9c81142cb77235a89c1e4
ShujiYamamoto <shuji.yamamoto@epkotsoftware.co.jp>
2023-08-29 14:51:31 +0900

    【修正】timestamp() -> datetime() 変更

--- root/database/migrations/2023_08_21_161130_create_user_messages_table.php
+++ root/database/migrations/2023_08_21_161130_create_user_messages_table.php
@@ -19,8 +19,8 @@ return new class extends Migration
             $table->text('text')->nullable()->comment('本文');
             $table->text('draft')->nullable()->comment('下書き');
             $table->boolean('hidden')->default(0)->comment('表示・非表示');
-            $table->timestamp('deleted_at')->nullable()->comment('削除日時');
-            $table->timestamps();
+            $table->softDeletesDatetime();
+            $table->datetimes();
 
             $table->foreign('admin_id')->references('id')->on('admins');
             $table->foreign('user_id')->references('id')->on('users');
