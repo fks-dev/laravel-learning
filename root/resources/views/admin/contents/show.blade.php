c2d1c9dbec523312b60052f3d69984f391aabb94
ShujiYamamoto <shuji.yamamoto@epkotsoftware.co.jp>
2023-08-29 14:12:20 +0900

    【修正】show.blade.php 論理名変更による修正

--- root/resources/views/admin/contents/show.blade.php
+++ root/resources/views/admin/contents/show.blade.php
@@ -10,12 +10,12 @@
 
         <h2 class="m-3">{{ $content->title }}</h2>
 
-        @if ($content->url != null)
+        @if ($content->youtube_video_id != null)
         <div class="ratio ratio-16x9 mx-auto p-2" style="width: 80%; height: 80%">
             <iframe
             width="560"
             height="315"
-            src="https://www.youtube.com/embed/{{ $content->url }}"
+            src="https://www.youtube.com/embed/{{ $content->youtube_video_id }}"
             title="YouTube video player"
             allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture;"
             allowfullscreen
@@ -25,18 +25,18 @@
         @elseif ($content->text != null)
             {!! $content->text !!}
 
-        @elseif ($content->movie != null)
+        @elseif ($content->movie_file_path != null)
         <video controls>
-            <source src="{{ asset('storage/' . $content->movie ) }}" type="video/mp4">
+            <source src="{{ asset('storage/' . $content->movie_file_path ) }}" type="video/mp4">
         </video>
 
-        @elseif ($content->file != null)
+        @elseif ($content->document_file_path != null)
             <a href="{{ route('content.download', $content) }}" class="btn btn-primary">ダウンロード開始</a>
 
-        @elseif ($content->kind == 'テスト')
-            <p>制限時間　　　：　{{ $content->testTime }}分</p>
-            <p>合格する得点率：　{{ $content->testPer }}％</p>
-            <p>出題数　　　　：　{{ $content->testVol }}問</p>
+        @elseif ($content->content_type == 6)
+            <p>制限時間　　　：　{{ $content->time_limit_minutes }}分</p>
+            <p>合格する得点率：　{{ $content->passing_score_rate }}％</p>
+            <p>出題数　　　　：　{{ $content->amount_questions }}問</p>
         @endif
 
         <div class="mt-3">備考</div>
