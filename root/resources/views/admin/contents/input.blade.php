d6b90cb0b1cef3b642f6d5a335dc28e19b82cda2
ShujiYamamoto <shuji.yamamoto@epkotsoftware.co.jp>
2023-08-29 14:11:52 +0900

    【修正】input.blade.php 論理名変更による修正

--- root/resources/views/admin/contents/input.blade.php
+++ root/resources/views/admin/contents/input.blade.php
@@ -16,16 +16,16 @@
 <div class="row m-3" id="movie" style="display: none;">
     <label class="col-sm-2 col-form-label fw-bold" for="movie">動画</label>
     <div class="col-sm-10">
-        @if (@isset($content->movie))
-            <input class="form-control" type="file" name="movie" id="movie" accept="video/*">
+        @if (@isset($content->movie_file_path))
+            <input class="form-control" type="file" name="movie_file_path" id="movie" accept="video/*">
             <div class="mt-3">
                 <p>こちらの動画がすでに登録されています。</p>
                 <video controls>
-                    <source src="{{ asset('storage/' . $content->movie ) }}" type="video/mp4">
+                    <source src="{{ asset('storage/' . $content->movie_file_path ) }}" type="video/mp4">
                 </video>
             </div>
         @else
-            <input class="form-control" type="file" name="movie" id="movie" accept="video/*">
+            <input class="form-control" type="file" name="movie_file_path" id="movie" accept="video/*">
         @endif
     </div>
 </div>
@@ -33,12 +33,15 @@
 {{-- URL --}}
 <div class="row m-3" id="url" style="display: none;">
     <label class="col-sm-2 col-form-label fw-bold" for="url">URL</label>
-    <div class="col-sm-10">
-        @if (@isset($content->url))
-            <input class="form-control" type="url" name="url" id="url" value="https://youtu.be/{{ $content->url }}">
-        @else
-            <input class="form-control" type="url" name="url" id="url">
-        @endif
+        <div class="col-sm-10">
+            <div class="input-group">
+                <span class="input-group-text">https://www.youtube.com/watch?v=</span>
+                @if (@isset($content->youtube_video_id))
+                    <input class="form-control" type="text" id="url" name="youtube_video_id" value="{{ $content->youtube_video_id }}">
+                @else
+                    <input class="form-control" type="text" id="url" name="youtube_video_id">
+                @endif
+            </div>
     </div>
 </div>
 
@@ -46,42 +49,42 @@
 <div class="row m-3" id="file" style="display: none;">
     <label class="col-sm-2 col-form-label fw-bold" for="file">ファイル</label>
     <div class="col-sm-10">
-        @if (@isset($content->file))
-            <input class="form-control" type="file" name="file" id="file">
+        @if (@isset($content->document_file_path))
+            <input class="form-control" type="file" name="document_file_path" id="file">
             <div class="mt-3">
                 <p>こちらのファイルがすでに登録されています。</p>
-                <p>{{ str_replace('handout/', '', $content->file) }}</p>
+                <p>{{ str_replace('handout/', '', $content->document_file_path) }}</p>
             </div>
         @else
-            <input class="form-control" type="file" name="file" id="file">
+            <input class="form-control" type="file" name="document_file_path" id="file">
         @endif
     </div>
 </div>
 
 {{-- テスト --}}
 <div class="row m-3" id="test" style="display: none;">
-    <label class="col-sm-2 col-form-label fw-bold" for="test">制限時間（1−100分）</label>
+    <label class="col-sm-2 col-form-label fw-bold" for="time">制限時間（1−100分）</label>
     <div class="col-sm-10">
-        @if (@isset($content->testTime))
-            <input class="form-control" type="number" name="testTime" id="testTime" value="{{ $content->testTime }}">
+        @if (@isset($content->time_limit_minutes))
+            <input class="form-control" type="number" name="time_limit_minutes" id="time" min="1" max="100" value="{{ $content->time_limit_minutes }}">
         @else
-            <input class="form-control" type="number" name="testTime" id="testTime">
+            <input class="form-control" type="number" name="time_limit_minutes" id="time" min="1" max="100">
         @endif
     </div>
-    <label class="col-sm-2 col-form-label fw-bold" for="test">合格する得点率（1−100％）</label>
+    <label class="col-sm-2 col-form-label fw-bold" for="rate">合格する得点率（1−100％）</label>
     <div class="col-sm-10">
-        @if (@isset($content->testPer))
-            <input class="form-control" type="number" name="testPer" id="testPer" value="{{ $content->testPer }}">
+        @if (@isset($content->passing_score_rate))
+            <input class="form-control" type="number" name="passing_score_rate" id="rate" min="1" max="100" value="{{ $content->passing_score_rate }}">
         @else
-            <input class="form-control" type="number" name="testPer" id="testPer">
+            <input class="form-control" type="number" name="passing_score_rate" id="rate" min="1" max="100">
         @endif
     </div>
-    <label class="col-sm-2 col-form-label fw-bold" for="test">出題数（1−100問）</label>
+    <label class="col-sm-2 col-form-label fw-bold" for="amount">出題数（1−100問）</label>
     <div class="col-sm-10">
-        @if (@isset($content->testVol))
-            <input class="form-control" type="number" name="testVol" id="testVol" value="{{ $content->testVol }}">
+        @if (@isset($content->amount_questions))
+            <input class="form-control" type="number" name="amount_questions" id="amount" min="1" max="100" value="{{ $content->amount_questions }}">
         @else
-            <input class="form-control" type="number" name="testVol" id="testVol">
+            <input class="form-control" type="number" name="amount_questions" id="amount" min="1" max="100">
         @endif
     </div>
 </div>
