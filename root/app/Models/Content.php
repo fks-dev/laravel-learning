c2c8dd84418b221f1113273239eb05d37567b0d3
ShujiYamamoto <shuji.yamamoto@epkotsoftware.co.jp>
2023-08-29 11:44:31 +0900

    【修正】Model 物理名の変更による修正、URLを切り分ける処理削除

--- root/app/Models/Content.php
+++ root/app/Models/Content.php
@@ -17,16 +17,16 @@ class Content extends Model
         'course_id',
         'admin_id',
         'title',
-        'kind',
+        'content_type',
         'text',
-        'movie',
-        'url',
-        'file',
+        'youtube_video_id',
+        'movie_file_path',
+        'document_file_path',
         'remarks',
-        'public',
-        'testTime',
-        'testPer',
-        'testVol',
+        'is_public',
+        'time_limit_minutes',
+        'passing_score_rate',
+        'amount_questions',
     ];
 
     public function course()
@@ -34,42 +34,14 @@ class Content extends Model
         return $this->belongsTo(Course::class);
     }
 
-    // URLの一部だけを格納
-    public function setUrlAttribute($value)
-    {
-        // YouTubeのビデオIDを抽出して格納します
-        $videoId = $this->getYouTubeVideoId($value);
-        $this->attributes['url'] = $videoId;
-    }
-
-    // YouTubeのビデオIDを取得するメソッド
-    private function getYouTubeVideoId($url)
-    {
-        $parsedUrl = parse_url($url);
-        if (isset($parsedUrl['host']) && $parsedUrl['host'] === 'youtu.be') {
-            // youtu.beの場合はパスの部分がビデオIDとなります
-            $path = ltrim($parsedUrl['path'], '/');
-            return $path;
-        } elseif (isset($parsedUrl['query'])) {
-            parse_str($parsedUrl['query'], $query);
-            if (isset($query['v'])) {
-                // クエリパラメータにvがある場合もビデオIDとなります
-                return $query['v'];
-            }
-        }
-
-        // ビデオIDが見つからない場合は空文字を返します
-        return $url;
-    }
-
-    // 動画が論理削除されたタイミングでstorageの動画ファイルを削除する
     protected static function boot()
     {
         parent::boot();
 
+        // 動画かファイルが論理削除されたタイミングでstorageの動画ファイルを削除する
         static::deleting(function ($content) {
-            $moviePath = $content->movie;
-            $filePath = $content->file;
+            $moviePath = $content->movie_file_path;
+            $filePath = $content->document_file_path;
             // 動画ファイルのパスと、storage内の動画が同じ場合に削除
             if ($moviePath && Storage::disk('public')->exists($moviePath)) {
                 Storage::disk('public')->delete($moviePath);
