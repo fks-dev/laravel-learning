8f0444e9845ade1684c42afc973d51bae5df7526
ShujiYamamoto <shuji.yamamoto@epkotsoftware.co.jp>
2023-09-11 18:04:49 +0900

    【追記】web.php route('group.show')

--- root/routes/web.php
+++ root/routes/web.php
@@ -42,6 +42,7 @@ Route::middleware('auth:admin')->group(function () {
             Route::get('', 'index')->name('.index');
             Route::get('create', 'create')->name('.create');
             Route::post('', 'store')->name('.store');
+            Route::get('{group}', 'show')->name('.show');
             Route::get('{group}/edit', 'edit')->name('.edit');
             Route::patch('{group}', 'update')->name('.update');
             Route::delete('{group}', 'destroy')->name('.destroy');
