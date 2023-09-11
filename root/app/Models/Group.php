2dc4fe9e7f0867d291318663f62accd4676f8e58
ShujiYamamoto <shuji.yamamoto@epkotsoftware.co.jp>
2023-09-11 17:54:51 +0900

    【修正】GroupModel グループ削除でコースとの紐付けも論理削除

--- root/app/Models/Group.php
+++ root/app/Models/Group.php
@@ -14,15 +14,41 @@ class Group extends Model
     protected $fillable = [
         'group_name',
         'remarks',
+        'updated_at',
     ];
 
     public function Courses()
     {
-        return $this->belongsToMany(Course::class, 'groups_courses', 'group_id', 'course_id');
+        return $this->belongsToMany(Course::class, 'groups_courses', 'group_id', 'course_id')->withTimestamps();
     }
 
     public function Users()
     {
-        return $this->belongsToMany(User::class, 'users_groups', 'group_id', 'user_id');
+        return $this->belongsToMany(User::class, 'users_groups', 'group_id', 'user_id')->withTimestamps();
+    }
+
+    // ユーザーが削除された時に、IDに紐づく中間テーブルの値も削除される
+    protected static function boot()
+    {
+        parent::boot();
+        static::deleting(function ($group) {
+            $group->UsersGroupsTable()->delete();
+        });
+
+        static::deleting(function ($group) {
+            $group->GroupsCoursesTable()->delete();
+        });
+    }
+
+    // users_groupsテーブルとのリレーション
+    public function UsersGroupsTable()
+    {
+        return $this->hasMany(UsersGroup::class, 'group_id', 'id');
+    }
+
+    // groups_coursesテーブルとのリレーション
+    public function GroupsCoursesTable()
+    {
+        return $this->hasMany(GroupsCourse::class, 'group_id', 'id');
     }
 }
