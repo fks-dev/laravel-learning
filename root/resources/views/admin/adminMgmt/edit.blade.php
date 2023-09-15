f199b3371f2f0b5154a6b5af6cce1f2e9701eab1
ShujiYamamoto <shuji.yamamoto@epkotsoftware.co.jp>
2023-09-15 13:17:51 +0900

    AdminMgmt Edit view

--- root/resources/views/admin/adminMgmt/edit.blade.php
+++ root/resources/views/admin/adminMgmt/edit.blade.php
@@ -6,11 +6,11 @@
 </head>
 <body>
     <div class="mt-3 container">
-        <a href="{{ route('adminMgmt.index') }}">&lt;&lt;戻る</a>
+        <a href="{{ route('admin.adminMgmt.index') }}">&lt;&lt;戻る</a>
         <div class="border">
             <div class="p-2 bg-secondary text-white">管理者編集</div>
 
-            <form action="{{ route('adminMgmt.update', $admin) }}" method="post">
+            <form action="{{ route('admin.adminMgmt.update', $admin) }}" method="post">
                 @csrf
                 @method('patch')
                 <div class="mx-5 px-5">
@@ -30,7 +30,7 @@
                             <span class="text-danger fw-bold">＊</span>
                         </label>
                         <div class="col-sm-9">
-                            <a href="{{ route('adminMgmt.password', $admin) }}" class="btn btn-success form-control">
+                            <a href="{{ route('admin.adminMgmt.password', $admin) }}" class="btn btn-success form-control">
                                 パスワードの変更
                             </a>
                         </div>
