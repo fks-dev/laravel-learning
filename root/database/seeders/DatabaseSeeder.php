ec149495b5e239cf241d32a29d4476383f3bd09a
ShujiYamamoto <shuji.yamamoto@epkotsoftware.co.jp>
2023-09-19 13:38:27 +0900

    DatabaseSeeder add InformationSeeder::class

--- root/database/seeders/DatabaseSeeder.php
+++ root/database/seeders/DatabaseSeeder.php
@@ -16,6 +16,7 @@ class DatabaseSeeder extends Seeder
             AdminSeeder::class,
             UserSeeder::class,
             ContentSeeder::class,
+            InformationSeeder::class,
         ]);
     }
 }
