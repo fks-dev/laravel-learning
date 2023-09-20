8a7f3b4cab9fed3fafa1c0d87f56353c4b48369b
ShujiYamamoto <shuji.yamamoto@epkotsoftware.co.jp>
2023-09-20 14:06:42 +0900

    【削除】Admin Model  use BelongsToMany

--- root/app/Models/Admin.php
+++ root/app/Models/Admin.php
@@ -4,7 +4,6 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
-use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Admin extends Authenticatable
