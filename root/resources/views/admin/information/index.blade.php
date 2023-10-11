05cc699c46094354c0aa175e63ea5dac45fe0dd4
hibiki-w <wakanohibiki.work@gmail.com>
2023-10-11 13:50:10 +0900

    お知らせ一覧にメニューとログアウトボタンを追加

--- root/resources/views/admin/information/index.blade.php
+++ root/resources/views/admin/information/index.blade.php
@@ -1,29 +1,33 @@
 <!DOCTYPE html>
 <html lang="ja">
+
 <head>
     @include('admin.head')
     @include('admin.sort')
     <link rel="stylesheet" href="/css/information.css">
     <title>お知らせ一覧</title>
 </head>
+
 <body>
     <div class="mt-5 container">
+        @include('admin.logoutBtn')
+        @include('admin.menu')
         <div class="d-flex justify-content-between">
             <h2 class="col">お知らせ一覧</h2>
             <div class="col-auto">
                 <a class="btn btn-primary" href="{{ route('admin.information.create')}}">&plus;追加</a>
             </div>
         </div>
-{{-- 登録・削除　メッセージ --}}
+        {{-- 登録・削除　メッセージ --}}
         <div id="message">
             @if (session('message'))
-                <div class="alert alert-success">
+            <div class="alert alert-success">
                 {{ session('message') }}
-                </div>
+            </div>
             @elseif (session('danger'))
             <div class="alert alert-danger">
                 {{ session('danger') }}
-                </div>
+            </div>
             @endif
         </div>
 
@@ -35,30 +39,29 @@
                     <th class="col-2 text-center sort" data-sort="asc">作成日時</th>
                     <th class="col-2 text-center sort" data-sort="asc">更新日時</th>
                     <th class="col-2 text-center">Actions</th>
-                    </tr>
+                </tr>
             </thead>
 
             <tbody>
 
                 @foreach ($informations as $information)
-                    <tr data-id="{{ $information->id }}">
-                        <td class="align-middle">{{ $information->title }}</td>
-                        <td class="align-middle text-center"></td>
-                        <td class="align-middle text-center">{{ $information->created_at }}</td>
-                        <td class="align-middle text-center">{{ $information->updated_at }}</td>
+                <tr data-id="{{ $information->id }}">
+                    <td class="align-middle">{{ $information->title }}</td>
+                    <td class="align-middle text-center"></td>
+                    <td class="align-middle text-center">{{ $information->created_at }}</td>
+                    <td class="align-middle text-center">{{ $information->updated_at }}</td>
 
-                        <td class="text-center">
-                            <a class="btn btn-success" href="{{ route('admin.information.edit', $information) }}">編集</a>
+                    <td class="text-center">
+                        <a class="btn btn-success" href="{{ route('admin.information.edit', $information) }}">編集</a>
 
-                            <form action="{{ route('admin.information.destroy', $information) }}" method="post" class="d-inline">
-                                @csrf
-                                @method('delete')
-                                <input class="btn btn-danger" type="submit" value="削除"
-                                onClick="return confirm('本当に削除しますか？');">
-                            </form>
+                        <form action="{{ route('admin.information.destroy', $information) }}" method="post" class="d-inline">
+                            @csrf
+                            @method('delete')
+                            <input class="btn btn-danger" type="submit" value="削除" onClick="return confirm('本当に削除しますか？');">
+                        </form>
 
-                        </td>
-                    </tr>
+                    </td>
+                </tr>
                 @endforeach
 
             </tbody>
@@ -66,4 +69,5 @@
         </table>
     </div>
 </body>
+
 </html>
