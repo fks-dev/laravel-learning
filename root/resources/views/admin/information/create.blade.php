ad82f528db551a711f48567898577408bd032457
hibiki-w <wakanohibiki.work@gmail.com>
2023-10-11 14:19:22 +0900

    お知らせ新規作成画面

--- root/resources/views/admin/information/create.blade.php
+++ root/resources/views/admin/information/create.blade.php
@@ -1,9 +1,11 @@
 <!DOCTYPE html>
 <html lang="ja">
+
 <head>
     @include('admin.head')
     <title>新規お知らせ登録</title>
 </head>
+
 <body>
     <div class="mt-3 container">
         <a href="{{ route('admin.information.index') }}">&lt;&lt;戻る</a>
@@ -33,10 +35,12 @@
                     <div class="row m-3">
                         <label class="col-sm-2 col-form-label fw-bold">対象グループ</label>
                         <div class="col-sm-10">
-                            <select class="form-control" name="group" id="group">
-                                <option value="1">グループ1</option>
+                            <select class="form-select" name="group[]" id="group" multiple>
+                                @foreach($groups as $group)
+                                <option value="{{ $group->id }}">{{ $group->group_name }}</option>
+                                @endforeach
                             </select>
-                            <input class="form-contorl btn btn-primary mt-3" type="submit" value="登録">
+                            <input class="form-control btn btn-primary mt-3" type="submit" value="登録">
                         </div>
                     </div>
 
@@ -45,4 +49,5 @@
         </div>
     </div>
 </body>
+
 </html>
