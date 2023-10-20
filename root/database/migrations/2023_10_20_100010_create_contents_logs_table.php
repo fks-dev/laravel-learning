2eb8c9941a8588f7800ca01fd5c8be6afbcb18a3
hibiki-w <wakanohibiki.work@gmail.com>
2023-10-20 11:44:57 +0900

    contents_logsテーブル作成

---
+++ root/database/migrations/2023_10_20_100010_create_contents_logs_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('contents_logs', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->bigInteger('user_id');
+            $table->bigInteger('content_id');
+            $table->boolean('completed');
+
+            //外部キー制約
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->foreign('content_id')->references('id')->on('contents')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('contents_logs');
+    }
+};
