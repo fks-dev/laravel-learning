f0a61c2962b864ebf275982920156cd624d8ed40
Tanaka.T <toshiki.tanaka@epkotsoftware.co.jp>
2023-10-27 10:48:44 +0900

    UserMgmtControllerをUserManagementControllerに修正

--- root/app/Http/Controllers/UserManagementController.php
+++ root/app/Http/Controllers/UserManagementController.php
@@ -13,7 +13,7 @@ use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
 
 
-class UserMgmtController extends Controller
+class UserManagementController extends Controller
 {
     /**
      * Display a listing of the resource.
