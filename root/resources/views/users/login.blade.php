fe0b3d2896de925ddca409aa24fb20a6f890abfd
Yuki Ito <yuki.ito@epkotsoftware.co.jp>
2023-10-27 13:24:23 +0900

    【追記】body記載

--- root/resources/views/users/login.blade.php
+++ root/resources/views/users/login.blade.php
@@ -19,46 +19,50 @@
     </div>
 
     {{-- body --}}
-    <div id="container">
-        <div id="content" class="row">
-            <div class="users_login">
-                <div class="panel panel-info form-signin">
-                    <div class="panel-heading">ユーザーログイン画面</div>
-                    <div class="panel-body">
-                        <form action="{{ route('users.login') }}" method="POST">
-                            @csrf
-                            <div class="form-group">
-                                <div class="required">
-                                    <label for="username">
-                                        ユーザー名
-                                    </label>
-                                    <div class="input text required">
-                                        <input type="text" name="username" id="username" class="form-control" required />
+    <body>
+        <div id="container">
+            <div id="content" class="row">
+                <div class="users-login">
+                    <div class="panel panel-info form-signin">
+                        <div class="panel-heading text-bg-info p-3">ユーザーログイン画面</div>
+                        <div class="panel-body text-bg-light p-3">
+                            <form action="{{ route('users.login') }}" method="POST">
+                                @csrf
+                                <div class="form-group">
+                                    <div class="required">
+                                        <label for="username">
+                                            ユーザー名
+                                        </label>
+                                        <div class="input text required">
+                                            <input type="text" name="username" id="username" class="form-control" required />
+                                        </div>
                                     </div>
                                 </div>
-                            </div>
-                            <div class="form-group">
-                                <div class="required">
-                                    <label for="password">
-                                        パスワード
-                                    </label>
-                                    <div class="input text required">
-                                        <input type="password" name="password" id="password" class="form-control" required />
+                                <div class="form-group">
+                                    <div class="required">
+                                        <label for="password">
+                                            パスワード
+                                        </label>
+                                        <div class="input password required">
+                                            <input type="password" name="password" id="password" class="form-control" required />
+                                        </div>
                                     </div>
+                                    <input type="checkbox" id="remember_me" value="1" checked="checked">
+                                    ログイン状態を保持
                                 </div>
-                            </div>
-                            <div class="submit">
-                                @error('failed')
-                                <p style="color:red">{{ $message }}</p>
-                                @enderror
-                                <button class="btn btn-lg btn-primary btn-block" type="submit">ログイン</button>
-                            </div>
-                        </form>
+                                <div class="submit">
+                                    @error('failed')
+                                    <p style="color:red">{{ $message }}</p>
+                                    @enderror
+                                    <button class="btn btn-lg btn-primary btn-block" type="submit">ログイン</button>
+                                </div>
+                            </form>
+                        </div>
                     </div>
                 </div>
-            </div>
 
+            </div>
         </div>
-    </div>
+    </body>
 
 </html>
