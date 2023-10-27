502e981d90415cf4920c09ae2ceb14d6e0959ea9
Yuki Ito <yuki.ito@epkotsoftware.co.jp>
2023-10-27 17:13:51 +0900

    【修正】ログイン中、ログインページへ遷移するとエラーになる。避けるためにauthをissetに変更。

--- root/resources/views/header.blade.php
+++ root/resources/views/header.blade.php
@@ -3,11 +3,11 @@
         <div class="d-flex justify-content-between align-items-center container-fluid">
             <h2 class="navbar-brand ms-1 fs-3 text-white">laravel-learnig</h2>
             <ul class="nav me-2 text-white">
-                @auth
+                @isset($user)
                 <li class="nav-item border-end p-1">ようこそ{{ $user->username }}さん</li>
                 <li class="nav-item border-end p-1"><a class="link-underline text-white" href="#">設定</a></li>
                 <li class="nav-item p-1"><a class="link-underline text-white" href="{{ route('users.logout') }}">ログアウト</a></li>
-                @endauth
+                @endisset
             </ul>
         </div>
     </nav>
