f6f0e959573139f567adeaf404b4063fa04956f1
Yuki Ito <yuki.ito@epkotsoftware.co.jp>
2023-10-27 10:36:32 +0900

    【修正】ヘッダー部分切り出しに伴う変更

--- root/resources/views/users/index.blade.php
+++ root/resources/views/users/index.blade.php
@@ -10,19 +10,7 @@
 </head>
 
 <body>
-    <header>
-        <nav class="navbar p-0 bg-primary  ">
-            <div class="d-flex justify-content-between align-items-center container-fluid">
-                <h2 class="navbar-brand ms-1 fs-3 text-white">laravel-learnig</h2>
-                <ul class="nav me-2 text-white">
-                    <li class="nav-item border-end p-1">ようこそ{{ $user->username }}さん</li>
-                    <li class="nav-item border-end p-1"><a class="link-underline text-white" href="#">設定</a></li>
-                    <li class="nav-item p-1"><a class="link-underline text-white" href="{{ route('users.logout') }}">ログアウト</a></li>
-                </ul>
-            </div>
-        </nav>
-
-    </header>
+    @include('header')
     <main>
         <div class="container-md mt-4">
             <div class="card ">
