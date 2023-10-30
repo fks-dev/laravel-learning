f5dfdf68e87be8cc5aad0f52876f53dfaa992d71
Yuki Ito <yuki.ito@epkotsoftware.co.jp>
2023-10-30 13:34:27 +0900

    【追加】フッターを追加

--- root/resources/views/admin/login.blade.php
+++ root/resources/views/admin/login.blade.php
@@ -91,13 +91,28 @@
                     </div>
                 </div>
             </div>
-            <div>
-                @error('failed')
-                    <p style="color:red">{{ $message }}</p>
-                @enderror
-                <button type="submit">ログイン</button>
+        </div>
+
+    </body>
+
+    {{-- footer --}}
+    <footer>
+
+        <nav class="navbar fixed-bottom p-0 bg-primary">
+
+            <div class="d-flex align-items-center container-fluid
+            justify-content-center">
+
+                <div class="navbar-brand ms-1 fs-3 text-white text-center">
+                    © 2023 エプコットソフトウェア
+                </div>
+
             </div>
-        </form>
-    </div>
+
+        </nav>
+
+    </footer>
+
 </body>
+
 </html>
