65dcaf67d91060f079bb1066b6f535b312f44aa1
hibiki-w <wakanohibiki.work@gmail.com>
2023-11-01 14:29:03 +0900

    診断結果表示ページ作成(機能のみ)

---
+++ root/resources/views/users/recommend/answer.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    @include('admin.head')
+    <title>動画相性診断</title>
+</head>
+
+<body>
+<p>回答ありがとうございます。診断結果が出ました。</p>
+
+<p>あなたへのおすすめ動画は{{ $course->title }}です。</p>
+<a href="{{ route('users.contents.index', $course) }}">おすすめ動画に進む</a>
+</body>
