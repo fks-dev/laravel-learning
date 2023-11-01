c1964abfd0d119379c1eebbe740a67f84334761b
hibiki-w <wakanohibiki.work@gmail.com>
2023-11-01 16:11:20 +0900

    view内の誤字修正

--- root/resources/views/users/recommend/answer.blade.php
+++ root/resources/views/users/recommend/answer.blade.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html lang="ja">
 <head>
-    @include('admin.head')
+    @include('head')
     <title>動画相性診断</title>
 </head>
 
@@ -9,5 +9,5 @@
 <p>回答ありがとうございます。診断結果が出ました。</p>
 
 <p>あなたへのおすすめ動画は{{ $course->title }}です。</p>
-<a href="{{ route('users.contents.index', $course) }}">おすすめ動画に進む</a>
+<a href="{{ route('users.content.index', $course) }}">おすすめ動画に進む</a>
 </body>
--- root/resources/views/users/recommend/index.blade.php
+++ root/resources/views/users/recommend/index.blade.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html lang="ja">
 <head>
-    @include('admin.head')
+    @include('head')
     <title>動画相性診断</title>
 </head>
 
