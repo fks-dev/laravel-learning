d7ddaa147387548872a2b8befc9a1f1863abcddc
hibiki-w <wakanohibiki.work@gmail.com>
2023-11-02 11:27:29 +0900

    <head>タグ内の不要な記述を削除

--- root/resources/views/users/contents/show.blade.php
+++ root/resources/views/users/contents/show.blade.php
@@ -2,10 +2,7 @@
 <html lang="ja">
 
 <head>
-    <meta charset="UTF-8">
     @include('head')
-    <link rel="stylesheet" href="/css/user_index.css">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>{{ $title }}</title>
 </head>
 
