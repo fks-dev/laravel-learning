09c73c0d38d98b98e750406983fac0112e3ce878
hibiki-w <wakanohibiki.work@gmail.com>
2023-11-09 15:39:12 +0900

    ユーザートップ画面にCourse一覧情報を$coursesとして渡す処理

--- root/app/Http/Controllers/UserController.php
+++ root/app/Http/Controllers/UserController.php
@@ -20,6 +20,12 @@ class UserController extends Controller
         //重複を除外し、最新の5件のみ取得する
         $informations = $informations->unique('id')->sortByDesc('updated_at')->take(5);
 
-        return view('users.index', compact('user','informations'));
+        //ログイン中のユーザーに向けたコースを取得
+        $courses = collect();
+        foreach($groups as $group){
+            $courses = $courses->concat($group->courses);
+        }
+        $courses = $courses->unique('id')->sortBy('id');
+        return view('users.index', compact('user','informations','courses'));
     }
 }
