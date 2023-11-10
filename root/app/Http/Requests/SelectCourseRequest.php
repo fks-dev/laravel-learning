9e2c9ebbe4406fecc244e2624c4a32f517e5a58d
hibiki-w <wakanohibiki.work@gmail.com>
2023-11-10 11:18:26 +0900

    バリデーションエラー時の処理を記述

--- root/app/Http/Requests/SelectCourseRequest.php
+++ root/app/Http/Requests/SelectCourseRequest.php
@@ -4,7 +4,6 @@ namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
 use Illuminate\Contracts\Validation\Validator;
-use Illuminate\Http\Exceptions\HttpResponseException;
 use Ramsey\Uuid\Type\Integer;
 
 class SelectCourseRequest extends FormRequest
@@ -32,6 +31,6 @@ class SelectCourseRequest extends FormRequest
 
     protected function failedValidation(Validator $validator)
     {
-        throw new HttpResponseException(response("不正なパラメータが入力されています。", 400));
+        abort(400);
     }
 }
