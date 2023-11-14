476699a07795433eb3b5715a60d6d6daef5526ea
Tanaka.T <toshiki.tanaka@epkotsoftware.co.jp>
2023-11-14 11:30:37 +0900

    admin配下にheader作成

---
+++ root/resources/views/admin/header.blade.php
@@ -0,0 +1,14 @@
+<header>
+    <nav class="navbar p-0 bg-primary  ">
+        <div class="d-flex justify-content-between align-items-center container-fluid">
+            <h2 class="navbar-brand ms-1 fs-3 text-white">laravel-learnig</h2>
+            <ul class="nav me-2 text-white">
+                @isset($user)
+                <li class="nav-item border-end p-1">ようこそ{{ $user->username }}さん</li>
+                <li id="setting" class="nav-item border-end p-1" ><a class="link-underline text-white" href="{{ route('users.password.index') }}">パスワード変更</a></li>
+                <li class="nav-item p-1"><a class="link-underline text-white" href="{{ route('admin.logout') }}">ログアウト</a></li>
+                @endisset
+            </ul>
+        </div>
+    </nav>
+</header>
