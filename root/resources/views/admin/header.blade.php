18aa2b149c61893a4c56e04a400791af24669c17
Tanaka.T <toshiki.tanaka@epkotsoftware.co.jp>
2023-11-15 11:21:53 +0900

    【修正】@issetに渡される値をuserからloggedInAdminに修正

--- root/resources/views/admin/header.blade.php
+++ root/resources/views/admin/header.blade.php
@@ -3,9 +3,9 @@
         <div class="d-flex justify-content-between align-items-center container-fluid">
             <h2 class="navbar-brand ms-1 fs-3 text-white">laravel-learnig</h2>
             <ul class="nav me-2 text-white">
-                @isset($user)
-                <li class="nav-item border-end p-1">ようこそ{{ $user->username }}さん</li>
-                <li id="setting" class="nav-item border-end p-1" ><a class="link-underline text-white" href="{{ route('admin.admin-management.password', $user) }}">パスワード変更</a></li>
+                @isset($loggedInAdmin)
+                <li class="nav-item border-end p-1">ようこそ{{ $loggedInAdmin->username }}さん</li>
+                <li id="setting" class="nav-item border-end p-1" ><a class="link-underline text-white" href="{{ route('admin.admin-management.password', $loggedInAdmin) }}">パスワード変更</a></li>
                 <li class="nav-item p-1"><a class="link-underline text-white" href="{{ route('admin.logout') }}">ログアウト</a></li>
                 @endisset
             </ul>
