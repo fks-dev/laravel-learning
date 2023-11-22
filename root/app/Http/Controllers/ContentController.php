b4f7462a5630f1a5fb3b0fb8f57392f20b935023
anna-nouchi-epkotsoftware <anna.nouchi@epkotsoftware.co.jp>
2023-11-22 14:17:26 +0900

    【修正】コンテンツ種別変更に伴うContentControllerのstoreメソッドの修正

--- root/app/Http/Controllers/ContentController.php
+++ root/app/Http/Controllers/ContentController.php
@@ -59,44 +59,16 @@ class ContentController extends Controller
      */
     public function store(StoreContentRequest $request, $course)
     {
-        $content_type = $request->input('content_type');
-        $user = Auth::guard('admin')->user();
+        $user = Auth::user();
 
         $data = [
-            'course_id'    => $request->course_id,
-            'admin_id'     => $user->id,
-            'title'        => $request->title,
-            'content_type' => $content_type,
-            'is_public'    => $request->is_public,
+            'course_id'        => $request->course_id,
+            'admin_id'         => $user->id,
+            'title'            => $request->title,
+            'youtube_video_id' => $request->youtube_video_id,
+            'remarks'          => $request->remarks,
+            'is_public'        => $request->is_public,
         ];
-
-        if ($content_type != 1) {
-            $data['remarks'] = $request->remarks;
-
-            if ($content_type == 4) { //リッチテキスト
-                $data['text'] = $request->text;
-
-            } elseif ($content_type == 5) { //動画ファイル
-                $movie         = $request->file('movie_file_path');
-                $movieName     = time() . '_' . $movie->getClientOriginalName();
-                $moviePath     = $movie->storeAs('movies', $movieName, 'public');
-                $data['movie_file_path'] = $moviePath;
-
-            } elseif ($content_type == 2) { //URL埋め込み
-                $data['youtube_video_id'] = $request->youtube_video_id;
-
-            }elseif ($content_type == 3) { //配布資料
-                $file         = $request->file('document_file_path');
-                $fileName     = time() . '_' . $file->getClientOriginalName();
-                $filePath     = $file->storeAs('handout', $fileName, 'public');
-                $data['document_file_path'] = $filePath;
-
-            } elseif ($content_type == 6) {
-                $data['time_limit_minutes'] = $request->time_limit_minutes;
-                $data['passing_score_rate'] = $request->passing_score_rate;
-                $data['amount_questions']   = $request->amount_questions;
-            }
-        }
 
         Content::create($data);
 
