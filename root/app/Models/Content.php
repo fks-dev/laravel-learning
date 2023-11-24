a1b610e0e22f3378082385248cd2d5d8aa92c341
anna-nouchi-epkotsoftware <anna.nouchi@epkotsoftware.co.jp>
2023-11-24 14:27:55 +0900

    【修正】コンテンツ種別変更に伴うContent.phpのyoutube以外の処理を削除

--- root/app/Models/Content.php
+++ root/app/Models/Content.php
@@ -5,7 +5,6 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
-use Illuminate\Support\Facades\Storage;
 
 
 class Content extends Model
@@ -45,20 +44,6 @@ class Content extends Model
     protected static function boot()
     {
         parent::boot();
-
-        // 動画かファイルが論理削除されたタイミングでstorageの動画ファイルを削除する
-        static::deleting(function ($content) {
-            $moviePath = $content->movie_file_path;
-            $filePath = $content->document_file_path;
-            // 動画ファイルのパスと、storage内の動画が同じ場合に削除
-            if ($moviePath && Storage::disk('public')->exists($moviePath)) {
-                Storage::disk('public')->delete($moviePath);
-
-                // ファイルのパスと、storage内のファイルが同じ場合に削除
-            } elseif ($filePath && Storage::disk('public')->exists($filePath)) {
-                Storage::disk('public')->delete($filePath);
-            }
-        });
 
         // 並び替え時に使用
         static::creating(function ($model) {
