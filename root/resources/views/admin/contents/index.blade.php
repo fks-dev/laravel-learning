05bc562010b15d627f3466a4cebb71dad871d4d2
anna-nouchi-epkotsoftware <anna.nouchi@epkotsoftware.co.jp>
2023-11-24 11:07:09 +0900

    【修正】コンテンツ種別変更に伴うadmin\contents\indexの修正

--- root/resources/views/admin/contents/index.blade.php
+++ root/resources/views/admin/contents/index.blade.php
@@ -43,7 +43,6 @@
             <thead>
                 <tr>
                     <th class="col-2">コンテンツ名</th>
-                    <th class="col-1 text-center">コンテンツ種別</th>
                     <th class="col-1 text-center">ステータス</th>
                     <th class="col-2 text-center">作成日時</th>
                     <th class="col-2 text-center">更新日時</th>
@@ -57,7 +56,6 @@
                         <td class="align-middle">
                             <a href="{{ route('admin.contents.show', $content->id) }}">{{ $content->title }}</a>
                         </td>
-                        <td class="align-middle text-center">@include('admin.contents.contentType')</td>
                         <td class="align-middle text-center">{{ $content->is_public == 1 ? '公開' : '非公開' }}</td>
                         <td class="align-middle text-center">{{ $content->created_at }}</td>
                         <td class="align-middle text-center">{{ $content->updated_at }}</td>
