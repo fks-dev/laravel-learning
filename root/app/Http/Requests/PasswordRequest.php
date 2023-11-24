67d7ff0f16e434e08dfe45a082261117c79a134a
Tanaka.T <toshiki.tanaka@epkotsoftware.co.jp>
2023-11-24 13:45:25 +0900

    Revert "PasswordRequestファイルをUserPasswordRequestファイルにリネーム"
    This reverts commit 55328d685ebb581cbc55c5e949f879394309eed8.

--- root/app/Http/Requests/PasswordRequest.php
+++ root/app/Http/Requests/PasswordRequest.php
@@ -5,7 +5,7 @@ namespace App\Http\Requests;
 use Illuminate\Foundation\Http\FormRequest;
 use Illuminate\Support\Facades\Hash;
 
-class UserPasswordRequest extends FormRequest
+class PasswordRequest extends FormRequest
 {
     /**
      * Determine if the user is authorized to make this request.
