56a44c142b46251f447cbb1c13aeff58b99c088a
hibiki-w <wakanohibiki.work@gmail.com>
2023-11-29 14:51:58 +0900

    ContentモデルのgetLogメソッドが消失していた問題を修正

--- root/app/Models/Content.php
+++ root/app/Models/Content.php
@@ -41,6 +41,16 @@ class Content extends Model
         return $this->hasMany(ContentsLog::class);
     }
 
+    public function getLog($user)
+    {
+        $content_id = $this->id;
+        if (ContentsLog::where('user_id', $user->id)->where('content_id', $content_id)->exists()) {
+            return ContentsLog::where('user_id', $user->id)->where('content_id', $content_id)->first();
+        } else {
+            return null;
+        }
+    }
+
     protected static function boot()
     {
         parent::boot();
@@ -49,6 +59,5 @@ class Content extends Model
         static::creating(function ($model) {
             $model->position = Content::max('position') + 1;
         });
-
     }
 }
