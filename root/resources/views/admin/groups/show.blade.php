f38506cbbe2828ad65152233ed90f7d527491ef7
Tomoki-A-epkotsoftware <tomoki.aono@epkotsoftware.co.jp>
2023-11-30 13:45:28 +0900

    戻るボタンのレイアウト変更
    表示テキストを戻る→グループ一覧に変更
    bootstrapの記述追記で背景色、ボーダー、マージン、パディングクラスを適用

--- root/resources/views/admin/groups/show.blade.php
+++ root/resources/views/admin/groups/show.blade.php
@@ -7,7 +7,9 @@
 <body>
     @include('admin.header')
     <div class="container mt-3 border">
-    <a href="{{ route('admin.groups.index') }}">&lt;&lt;戻る</a>
+        <div class="mb-4 p-3 border rounded" style="background-color: #f5f5f5;">
+            <a class="text-decoration-none" href="{{ route('admin.groups.index') }}">グループ一覧</a>
+        </div>
         <h3 class="rounded-top p-2 card-header text-success shadow-sm" style="background-color: #cdeee0">{{ $group->group_name }}</h3>
         <div class="d-flex">
             <div class="p-3">
