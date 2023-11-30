e5bd06b91a2f6738307f175800ce799714c32403
anna-nouchi-epkotsoftware <anna.nouchi@epkotsoftware.co.jp>
2023-11-30 10:46:33 +0900

    users/contents/index.bladeにHOMEボタン実装と見た目をお知らせと合わせた

--- root/resources/views/users/contents/index.blade.php
+++ root/resources/views/users/contents/index.blade.php
@@ -8,13 +8,16 @@
 
 <body>
     @include('users.header')
-    <main>
-        <div class="border rounded m-2">
-            <div class="rounded-top p-2">
+    <main class="container-md mt-4">
+        <div class="mb-4 p-3 border rounded" style="background-color: #f5f5f5;">
+            <a class="text-decoration-none" href="{{ route('users.index') }}">HOME</a>
+        </div>
+        <div class="border rounded">
+            <div class="rounded-top p-2 card-header text-primary shadow-sm" style="background-color: #d3e6fd">
                 <p>{{ $course_title }}</p>
             </div>
             <div class="m-2">
-                <table class="table p-2 bg-prime">
+                <table class="table table-striped table-hover">
                     <thead>
                         <tr>
                             <th>タイトル</th>
@@ -23,10 +26,10 @@
                             <th>完了</th>
                         </tr>
                     </thead>
-                    <tbody>
+                    <tbody class="fw-bold">
                         @foreach($contents as $content)
                         <tr>
-                            <td><a href="{{ route('users.contents.show', $content) }}">{{$content->title}}</a></td>
+                            <td><a class="none-underline" href="{{ route('users.contents.show', $content) }}">{{$content->title}}</a></td>
                             @if($content->getLog($user))
                             <td><p>{{ $content->getLog($user)->created_at }}</p></td>
                             <td><p>{{ $content->getLog($user)->updated_at }}</p></td>
@@ -43,9 +46,6 @@
             </div>
         </div>
     </main>
-    <footer>
-
-    </footer>
 </body>
 
 </html>
