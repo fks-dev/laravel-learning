d65011ce1182f0c2caf673fc68ad4ade91b170b4
hibiki-w <wakanohibiki.work@gmail.com>
2023-12-01 17:28:22 +0900

    ユーザー管理機能の一覧画面から所属コース欄を削除

--- root/app/Http/Controllers/UserManagementController.php
+++ root/app/Http/Controllers/UserManagementController.php
@@ -15,9 +15,6 @@ use Illuminate\Support\Facades\Validator;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
-
-
-
 class UserManagementController extends Controller
 {
     /**
@@ -25,19 +22,12 @@ class UserManagementController extends Controller
      */
     public function index()
     {
-        $users = User::with('groups.courses')->get();
+        $users = User::with('groups')->get();
         $logins = UserLogin::all();
         $adminUser = Auth::user();
 
-        $userCourses = collect();
-        foreach($users as $user){
-            if($user->groups->isEmpty()){
-                continue; //ユーザーの対象グループがないなら今回のループをスキップする
-            }
-            $userCourses[$user->id] = $user->groups->flatMap->courses->unique('id');
-        }
 
-        return view('admin.user-management.index', compact('users','userCourses', 'logins', 'adminUser'));
+        return view('admin.user-management.index', compact('users', 'logins', 'adminUser'));
     }
 
     // ユーザ側のパスワード変更画面
@@ -134,7 +124,7 @@ class UserManagementController extends Controller
         $user->groups()->sync($groupIds);
 
 
-        return redirect()->route('admin.user-management.index')->with('message', $request->username.'の情報を更新しました');
+        return redirect()->route('admin.user-management.index')->with('message', $request->username . 'の情報を更新しました');
     }
 
     /**
