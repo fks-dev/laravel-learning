85f5698e3c0b9ebf6bdfe732f1fc4e090fec352e
Kotaro Murase <kotaro.murase@epkotsoftware.co.jp>
2023-12-04 15:17:33 +0900

    さらに不要な箇所を削除

--- root/resources/views/welcome.blade.php
+++ root/resources/views/welcome.blade.php
@@ -2,19 +2,17 @@
 <html lang="ja">
 
 <head>
-  @include('head')
+    @include('head')
 </head>
 
-<body class="d-flex text-center text-bg-light">
-  <div class="p-3 mx-auto">
-    <main>
-      <h1>Laravel-learing</h1>
-      <p class="lead">Laravel-learningのデフォルトページです。</p>
-      <p class="lead">
-        <a href="{{ route('admin.login.index') }}" class="btn btn-primary">ログイン画面へ</a>
-      </p>
-    </main>
-  </div>
+<body class="text-center text-bg-light">
+    <div class="p-3 mx-auto">
+        <main>
+            <h1>Laravel-learing</h1>
+            <p class="lead">Laravel-learningのデフォルトページです。</p>
+            <a href="{{ route('admin.login.index') }}" class="btn btn-primary">ログイン画面へ</a>
+        </main>
+    </div>
 </body>
 
 </html>
