133da97e1eeab6bffcfc40fc72ae9edfd4052584
hibiki-w <wakanohibiki.work@gmail.com>
2023-12-04 11:39:30 +0900

    AdminManagementControllerからCSV関係の機能を削除

--- root/app/Http/Controllers/AdminManagementController.php
+++ root/app/Http/Controllers/AdminManagementController.php
@@ -9,7 +9,6 @@ use App\Http\Requests\UpdateAdminRequest;
 use App\Models\Admin;
 use App\Models\AdminLogin;
 use Illuminate\Support\Facades\Hash;
-use Illuminate\Support\Facades\Validator;
 use Illuminate\Support\Facades\Auth;
 
 class AdminManagementController extends Controller
@@ -120,114 +119,5 @@ class AdminManagementController extends Controller
     {
         $admin->delete();
         return redirect()->route('admin.admin-management.index')->with('danger', $admin->username . 'を削除しました');
-    }
-
-    /**
-     * CSVファイルダウンロード
-     */
-    public function downloadCsv()
-    {
-        $fileName = 'admin.csv';
-        $csvRecords = self::getAdminCsvRecords();
-        return self::streamDownloadCsv($fileName, $csvRecords);
-    }
-
-    // レコード取得
-    private static function getAdminCsvRecords(): array
-    {
-        $admins = Admin::withTrashed()->get();
-        $csvRecords = [
-            ['ID', 'ユーザー名', 'メールアドレス', '削除日時', '作成日時', '更新日時'],
-        ];
-        foreach ($admins as $admin) {
-            $csvRecords[] = [
-                $admin->id,
-                $admin->username,
-                $admin->mail_address,
-                $admin->deleted_at,
-                $admin->created_at,
-                $admin->updated_at,
-            ];
-        }
-        return $csvRecords;
-    }
-
-    // CSV or TSV
-    private static function determineContentType(string $separator)
-    {
-        if ($separator === ',') {
-            return 'text/csv';
-        } elseif ($separator === "\t") {
-            return 'text/tab-separated-values';
-        }
-    }
-
-    // CSVストリームダウンロード
-    private static function streamDownloadCsv(
-        string $name,
-        iterable $fieldList,
-        string $separator = ',',
-        string $enclosure = '"',
-        string $escape = "\\",
-        string $eol = "\r\n",
-    ) {
-        $contentType = self::determineContentType($separator);
-        $headers = ['Content-Type' => $contentType];
-
-        return response()->streamDownload(function () use ($fieldList, $separator, $enclosure, $escape, $eol) {
-            $stream = fopen('php://output', 'w');
-            foreach ($fieldList as $fields) {
-                fputcsv($stream, $fields, $separator, $enclosure, $escape, $eol);
-            }
-            fclose($stream);
-        }, $name, $headers);
-    }
-
-    /**
-     * CSVインポート画面
-     */
-    public function createCsv()
-    {
-        $adminUser = Auth::user();
-        return view('admin.admin-management.import', compact('adminUser'));
-    }
-
-    /**
-     * CSVインポート
-     */
-    public function storeCsv(Request $request)
-    {
-        $validator = Validator::make($request->all(), [
-            'csv_file' => 'required | mimes:csv,txt',
-        ]);
-
-        if ($validator->fails()) {
-            return back()->withErrors($validator);
-        }
-
-        $file = $request->file('csv_file');
-        $handle = fopen($file, 'r');
-
-        if (!$handle) {
-            return redirect()->route('admin-management.index')->with('danger', 'CSVファイルを開けませんでした。');
-        }
-
-        // ヘッダー部分の読み込み
-        $length = 1000;
-        $header = fgetcsv($handle, $length, ',');
-
-        while (($data = fgetcsv($handle, $length, ',')) !== false) {
-            $username = $data[1];
-            $password = Hash::make('admin');
-            $mail_address = $data[2];
-
-            Admin::create([
-                'username' => $username,
-                'password' => $password,
-                'mail_address' => $mail_address,
-            ]);
-        }
-        fclose($handle);
-        return redirect()->route('admin.admin-management.index')->with('message', 'CSVファイルをインポートしました。');
     }
 }
