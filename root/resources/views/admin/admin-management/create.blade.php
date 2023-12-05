0f5cd01da5d9fc8aa164a22ceab0a0586d1b5791
Kotaro Murase <kotaro.murase@epkotsoftware.co.jp>
2023-12-05 12:04:02 +0900

    新規管理者登録画面から所属グループを削除

--- root/resources/views/admin/admin-management/create.blade.php
+++ root/resources/views/admin/admin-management/create.blade.php
@@ -46,10 +46,7 @@
                     </div>
 
                     <div class="row m-3">
-                        <label class="col-sm-3 col-form-label fw-bold" for="group">所属グループ</label>
                         <div class="col-sm-9">
-                            <input class="form-control" type="text" name="group" id="group">
-
                             {{-- 登録ボタン --}}
                             <input class="form-contorl btn btn-primary mt-3" type="submit" value="登録">
                         </div>
