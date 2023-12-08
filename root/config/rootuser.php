be82d88dae498df29cc650e79510f837b83895f8
hibiki-w <wakanohibiki.work@gmail.com>
2023-12-08 09:39:56 +0900

    ルート管理者に関するconfigファイル作成

---
+++ root/config/rootuser.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'root_admin' => [
+        'name' => env('ROOT_ADMIN_NAME', 'root'),
+        'password' => env('ROOT_ADMIN_PASSWORD', 'password'),
+        'email' => env('ROOT_ADMIN_EMAIL', 'root@admin')
+    ]
+];
