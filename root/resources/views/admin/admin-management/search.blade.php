dd4f520172d9d452268dc2062a1cafebc2cbdc66
hibiki-w <wakanohibiki.work@gmail.com>
2023-12-11 14:04:37 +0900

    検索結果のデータを出力するconsole.logの削除

--- root/resources/views/admin/admin-management/search.blade.php
+++ root/resources/views/admin/admin-management/search.blade.php
@@ -1,30 +1,28 @@
 <script>
-document.addEventListener("DOMContentLoaded", (event) => {
-    document.getElementById('searchForm').addEventListener('submit', async function(event) {
-        event.preventDefault();
+    document.addEventListener("DOMContentLoaded", (event) => {
+        document.getElementById('searchForm').addEventListener('submit', async function(event) {
+            event.preventDefault();
 
-        const formData = new FormData(this);
+            const formData = new FormData(this);
 
-        try {
-            const response = await fetch("{{ route('admin.admin-management.search') }}", {
-                method: 'POST',
-                body: formData,
-            });
+            try {
+                const response = await fetch("{{ route('admin.admin-management.search') }}", {
+                    method: 'POST',
+                    body: formData,
+                });
 
-            if (!response.ok) {
-                throw new Error('検索に失敗しました。');
-            }
-            const data = await response.json();
-            const tbody = document.querySelector('#sortable tbody');
-            tbody.innerHTML = '';
+                if (!response.ok) {
+                    throw new Error('検索に失敗しました。');
+                }
+                const data = await response.json();
+                const tbody = document.querySelector('#sortable tbody');
+                tbody.innerHTML = '';
 
-            console.log(data);
-
-            data.forEach((result) => {
-                const row = document.createElement('tr');
-                let date = new Date(result.created_at);
-                let formatDate = date.getFullYear() + "-" + ("0" + (date.getMonth() + 1)).slice(-2) + "-" + ("0" + date.getDate()).slice(-2) + " " + ("0" + date.getHours()).slice(-2) + ":" + ("0" + date.getMinutes()).slice(-2) + ":" + ("0" + date.getSeconds()).slice(-2);
-                row.innerHTML = `
+                data.forEach((result) => {
+                    const row = document.createElement('tr');
+                    let date = new Date(result.created_at);
+                    let formatDate = date.getFullYear() + "-" + ("0" + (date.getMonth() + 1)).slice(-2) + "-" + ("0" + date.getDate()).slice(-2) + " " + ("0" + date.getHours()).slice(-2) + ":" + ("0" + date.getMinutes()).slice(-2) + ":" + ("0" + date.getSeconds()).slice(-2);
+                    row.innerHTML = `
                     <td class="align-middle">${result.username}</td>
                     <td class="align-middle text-center">${result.mail_address}</td>
                     <td class="align-middle text-center"></td>
@@ -46,13 +44,13 @@ document.addEventListener("DOMContentLoaded", (event) => {
                     </td>
 
                 `;
-                tbody.appendChild(row);
-            });
+                    tbody.appendChild(row);
+                });
 
-        } catch (error) {
-            console.error(error);
-            alert('検索に失敗しました。');
-        }
+            } catch (error) {
+                console.error(error);
+                alert('検索に失敗しました。');
+            }
+        });
     });
-});
 </script>
