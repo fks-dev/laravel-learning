1e294c893c9f5c607cdae3036579825319ade328
Sora Sueshige <sora.sueshige@epkotsoftware.co.jp>
2023-12-14 16:12:24 +0900

    ユーザーのお知らせ閲覧機能のテストを作成：修正

--- root/tests/Feature/users/information/UsersInformationsTest.php
+++ root/tests/Feature/users/information/UsersInformationsTest.php
@@ -15,14 +15,49 @@ class UsersInformationsTest extends TestCase
 {
     use RefreshDatabase;
 
+    private $user;
+
+    public function setUp(): void
+    {
+        parent::setUp();
+
+        $this->user = User::factory()->create([
+            'username' => 'testUser',
+            'password' => Hash::make('testUser'),
+            'mail_address' => 'testUser@user.com',
+        ]);
+    }
+
     /**
      * @test
      */
-    public function test_users_informations_list_get_ok()
+    public function test_users_informations_get_ok()
+    {
+        $response = $this->actingAs($this->user)->get(route('users.informations.list'));
+
+        $response->assertStatus(200);
+    }
+
+    /**
+     * @test
+     */
+    public function test_users_informations_get_ok_unauthenticated()
+    {
+        Auth::logout();
+
+        $response = $this->get(route('users.informations.list'));
+
+        $response->assertStatus(302)->assertRedirect(route('users.login.index'));
+    }
+
+    /**
+     * @test
+     */
+    public function test_users_informations_get_ok_get_informations()
     {
         $user = User::factory()->create([
             'username' => 'testInfo',
-            'password' => Hash::make('testInfo'),
+            'password' => 'testInfo',
             'mail_address' => 'testInfo@test.com',
         ]);
 
@@ -31,16 +66,91 @@ class UsersInformationsTest extends TestCase
         $information = Information::factory()->create([
             'title' => 'testInfo',
             'text' => 'testInfo',
-            'admin_id' => $user->id,
+            'admin_id' => 1,
         ]);
 
         $group->informations()->attach($information);
 
-        $response = $this->actingAs($user)->get(route('users.informations.list'));
+        Auth::login($user);
 
-        //レスポンスが成功し、指定したテキストを含むことを確認
+        $response = $this->get(route('users.informations.list'));
+
         $response->assertStatus(200)->assertSee('testInfo');
     }
+
+    /**
+     * @test
+     */
+    public function test_users_informations_get_ok_sort()
+    {
+        $user = User::factory()->create([
+            'username' => 'testInfo',
+            'password' => Hash::make('testInfo'),
+            'mail_address' => 'testInfo@test.com',
+        ]);
+
+        Information::factory()->create([
+            'title' => 'testInfo',
+            'text' => 'testInfo',
+            'admin_id' => 1,
+            'updated_at' => now()->subDays(3),
+        ]);
+
+        Information::factory()->create([
+            'title' => 'testInfo',
+            'text' => 'testInfo',
+            'admin_id' => 1,
+            'updated_at' => now()->subDays(2),
+        ]);
+
+        Information::factory()->create([
+            'title' => 'testInfo',
+            'text' => 'testInfo',
+            'admin_id' => 1,
+            'updated_at' => now()->subDays(1),
+        ]);
+
+        Auth::login($user);
+
+        $response = $this->get(route('users.informations.list', ['sort' => 'updated_at']));
+
+        $response->assertStatus(200)->assertSeeInOrder(['testInfo']);
+    }
+
+    /**
+     * @test
+     */
+    public function test_users_informations_get_ok_no_duplicates()
+    {
+        Information::factory()->create([
+            'title' => 'testInfo1',
+            'text' => 'testInfo1',
+            'admin_id' => 1,
+        ]);
+
+        Information::factory()->create([
+            'title' => 'testInfo2',
+            'text' => 'testInfo2',
+            'admin_id' => 1,
+        ]);
+
+        Information::factory()->create([
+            'title' => 'testInfo3',
+            'text' => 'testInfo3',
+            'admin_id' => 1,
+        ]);
+
+        Auth::login($this->user);
+
+        $response = $this->get(route('users.informations.list'));
+
+        $response->assertStatus(200)
+            ->assertDontSee('testInfo1')
+            ->assertDontSee('testInfo2')
+            ->assertDontSee('testInfo3');
+    }
+
+
 
     /**
      * @test
