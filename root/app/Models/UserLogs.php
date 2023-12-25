d87a39050c8e1e8a6c6db1ae355b8d2f46a7e43b
shunsuke.hirano <shunsuke.hirano@epkotsoftware.co.jp>
2023-12-25 10:50:46 +0900

    UserLogsへ変更

--- root/app/Models/UserLogs.php
+++ root/app/Models/UserLogs.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class UserLogin extends Model
+class UserLogs extends Model
 {
     use HasFactory;
 
