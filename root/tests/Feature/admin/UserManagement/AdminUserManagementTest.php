f766b51cf4661f04ce571634807980ad0f94d7b2
Imura.R <ryuki.imura@epkotsoftware.co.jp>
2023-12-25 16:45:31 +0900

    AdminUserMagagementTest修正

--- root/tests/Feature/admin/UserManagement/AdminUserManagementTest.php
+++ root/tests/Feature/admin/UserManagement/AdminUserManagementTest.php
@@ -30,6 +30,15 @@ class AdminUserManagementTest extends TestCase
         ]);
     }
 
+    private function login()
+    {
+        $response = $this->post('/admin/login', [
+            'username' => 'testAdmin',
+            'password' => 'testAdmin',
+        ]);
+        return $response;
+    }
+
 
     /**
      * @test
@@ -38,9 +47,10 @@ class AdminUserManagementTest extends TestCase
 
      public function test_admin_user_management_index_ok()
     {
-        $response = $this->get(route('admin.user-management.index'));
-
+        $this->login();
+        $response = $this->get('/admin/user-management');
         $response->assertStatus(200);
+
         $response->assertViewIs('admin.user-management.index');
     }
 
@@ -51,18 +61,43 @@ class AdminUserManagementTest extends TestCase
 
     public function test_admin_user_management_post_ok()
     {
-        $response = $this->post(route('admin.user-management.store'), $this->user);
-        $response->assertRedirect(route('admin.user-management.index'));
+        $this->login();
+
+        // ランダムなユーザーIDを生成
+        $randomUserId = rand();
+
+        //モックデータを使用する
+        $userData = [
+            'username' => 'testUser',
+            'password' => 'password1',
+            'mail_address' => 'testUser1@user.com',
+        ];
+
+        //ユーザーIDを設定
+        $userDate['id'] = $randomUserId;
+        $response = $this->post('/admin/user-management', $userData);
+
+        //データベースに対するアサーション
+        $this->assertDatabaseHas('users', [
+            'username' => 'testUser',
+            'mail_address' => 'testUser1@user.com',
+        ]);
+        //パスワードのハッシュ検証
+        $this->assertTrue(Hash::check('password1', User::where('username', 'testUser')->first()->password));
+
+        $response->assertRedirect('/admin/user-management')->assertStatus(302)->assertSessionHas('message', 'testuserを登録しました');
     }
 
     /**
      * @test
-     * ユーザーの新規作成が正常に行われることを確認
+     * ユーザーの新規作成画面が正常に表示されることを確認
      */
 
     public function test_admin_user_management_create_get_ok()
     {
-        $response = $this->get(route('admin.user-management.create'));
+        $this->login();
+
+        $response = $this->get('/admin/user-management/create');
 
         $response->assertStatus(200);
         $response->assertViewIs('admin.user-management.create');
@@ -73,13 +108,22 @@ class AdminUserManagementTest extends TestCase
      * 検索機能が正常に行われることを確認
      */
 
-    public function test_admin_user_management_search_post_ok()
-    {
-        $response = $this->post(route('admin.user-management.search'), ['name' => $this->user->username]);
 
-        $response->assertStatus(200);
-        $response->assertJsonFragment(['name' => $this->user->username]);
-    }
+     public function test_admin_user_management_search_post_ok()
+     {
+        $this->login();
+
+         $response = $this->post('/admin/user-management/search', ['name' => $this->user->username]);
+
+         $response->assertJsonFragment(['name' => $this->user->username])
+             ->assertJsonStructure([
+                 'data' => [
+                     '*' => [
+                         'username',
+                     ]
+                 ],
+             ]);
+     }
 
      /**
      * @test
@@ -88,10 +132,22 @@ class AdminUserManagementTest extends TestCase
 
     public function test_admin_user_management_update_patch_ok()
     {
-        $newUsername = 'NewUsername';
+        $this->login();
 
-        $response = $this->patch(route('admin.user-management.update', $this->user), ['username' => $newUsername]);
-        $response->assertRedirect(route('admin.user-management.index'));
+        $newUsername = 'NewUsername';
+        $newMailaddress = 'newtestUser1@user.com';
+
+        $response = $this->patch("/admin/user-management/{$this->user->id}" , [
+            'username' => $newUsername,
+            'mail_address' => $newMailaddress,
+        ]);
+        $response->assertRedirect('/admin/user-management')->assertStatus(302);
+
+        $this->assertDatabaseHas('users', [
+            'id' => $this->user->id,
+            'username' => $newUsername,
+            'mail_address' => $newMailaddress,
+        ]);
     }
 
        /**
@@ -101,19 +157,25 @@ class AdminUserManagementTest extends TestCase
 
       public function test_admin_user_management_destroy_delete_ok()
       {
-        $response = $this->delete(route('admin.user-management.destroy', $this->user));
-        $response->assertRedirect(route('admin.user-management.index'));
+        $this->login();
+
+        $response = $this->delete("/admin/user-management/{$this->user->id}");
+        $response->assertRedirect('/admin/user-management')->assertStatus(302);
+
+        $this->assertDatabaseMissing( $this->user, ['id' => $this->user->id]);
       }
 
         /**
      * @test
-     * ユーザーの編集が正常に行われることを確認
+     * ユーザーの編集画面が正常に表示されることを確認
      */
       public function test_admin_user_management_edit_get_ok()
       {
-        $response = $this->get(route('admin.user-management.edit'));
+        $this->login();
+
+        $response = $this->get("/admin/user-management/{$this->user->id}/edit");
 
         $response->assertStatus(200);
-        $response->assertViewIs('admin.user-management.edit');
+        $response = $this->get('/admin/user-management/edit');
       }
 }
