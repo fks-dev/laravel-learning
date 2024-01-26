394a011faca33eb17344b9943e025c3736275804
Yokouchi. A <147117744+akiko-yokouchi-epkotsoftware@users.noreply.github.com>
2024-01-26 14:22:12 +0900

    レイアウトの追加修正

--- root/resources/views/admin/groups/show.blade.php
+++ root/resources/views/admin/groups/show.blade.php
@@ -6,61 +6,63 @@
 </head>
 <body>
     @include('admin.header')
-    <div class="container mt-3 border">
+    <div class="mt-5 container">
         <div class="mb-2">
             <a class="btn btn-secondary" href="{{ route('admin.groups.index') }}">戻る</a>
         </div>
-        <h3 class="rounded-top p-2 card-header text-success shadow-sm" style="background-color: #cdeee0">{{ $group->group_name }}</h3>
-        <div class="d-flex flex-column">
-            <div class="p-3">
-                <h4>所属コース</h4>
-                <div>
-                    <ul>
-                        @foreach ($group->courses as $course)
-                            <li class="align-items-center list-unstyled">{{ $course->title }}</li>
-                        @endforeach
-                    </ul>
-                </div>
-            </div>
-            <div class="p-3">
-                <h4>所属ユーザー</h4>
-                <div>
-                    <ul>
-                        @foreach ($group->users as $user)
-                            <li class="align-items-center list-unstyled">{{ $user->username }}</li>
-                        @endforeach
-                    </ul>
-                </div>
-            </div>
-            <div class="d-flex flex-column">
-                <div class="mt-2 p-3">
-                    <div class="fw-bold fs-5">作成日時</div>
-                    <div>{{ $group->created_at }}</div>
-                </div>
-                <div class="mt-2 p-3">
-                    <div class="fw-bold fs-5">更新日時</div>
-                    <div>{{ $group->updated_at }}</div>
-                </div>
-                <div class="mt-2 p-3">
-                    <div class="fw-bold fs-5">Action</div>
-                    <div class="action-btn">
-                        <a class="btn btn-success" href="{{ route('admin.groups.edit', ['group' => $group, 'show' => 'show']) }}">編集</a>
-
-                        <form action="{{ route('admin.groups.destroy', $group) }}" method="post" class="d-inline">
-                            @csrf
-                            @method('delete')
-                            <input class="btn btn-danger" type="submit" value="削除"
-                            onClick="return confirm('本当に削除しますか？');">
-                        </form>
+        <div class="mt-3">
+            <h3 class="rounded-top p-2 card-header text-success shadow-sm" style="background-color: #cdeee0;">{{ $group->group_name }}</h3>
+            <div class="border">
+                <div class="d-flex flex-column">
+                    <div class="p-3">
+                        <h4>受講コース</h4>
+                        <div>
+                            <ul>
+                                @foreach ($group->courses as $course)
+                                    <li class="align-items-center list-unstyled">{{ $course->title }}</li>
+                                @endforeach
+                            </ul>
+                        </div>
+                    </div>
+                    <div class="p-3">
+                        <h4>所属ユーザー</h4>
+                        <div>
+                            <ul>
+                                @foreach ($group->users as $user)
+                                    <li class="align-items-center list-unstyled">{{ $user->username }}</li>
+                                @endforeach
+                            </ul>
+                        </div>
+                    </div>
+                    <div class="d-flex flex-column">
+                        <div class="mt-2 p-3">
+                            <div class="fw-bold fs-5">作成日時</div>
+                            <div>{{ $group->created_at }}</div>
+                        </div>
+                        <div class="mt-2 p-3">
+                            <div class="fw-bold fs-5">更新日時</div>
+                            <div>{{ $group->updated_at }}</div>
+                        </div>
+                        <div class="mt-2 p-3">
+                            <div class="fw-bold fs-5">Action</div>
+                            <div class="action-btn">
+                                <a class="btn btn-success" href="{{ route('admin.groups.edit', ['group' => $group, 'show' => 'show']) }}">編集</a>
+                                <form action="{{ route('admin.groups.destroy', $group) }}" method="post" class="d-inline">
+                                    @csrf
+                                    @method('delete')
+                                    <input class="btn btn-danger" type="submit" value="削除" onClick="return confirm('本当に削除しますか？');">
+                                </form>
+                            </div>
+                        </div>
                     </div>
                 </div>
-            </div>
-        </div>
-        <div class="p-3">
-            <h3>備考</h3>
-            <div class="border">
-                <div class="p-2">
-                    <p>{!! nl2br(htmlspecialchars($group->remarks)) !!}</p>
+                <div class="p-3">
+                    <h4>備考</h4>
+                    <div class="border">
+                        <div class="p-2">
+                            <p>{!! nl2br(htmlspecialchars($group->remarks)) !!}</p>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
@@ -68,4 +70,5 @@
     {{-- footer --}}
     @include('footer')
 </body>
+
 </html>
--- root/resources/views/admin/informations/show.blade.php
+++ root/resources/views/admin/informations/show.blade.php
@@ -5,44 +5,47 @@
 </head>
 <body>
     @include('admin.header')
-    <div class="container mt-3 border">
+    <div class="mt-5 container">
         <div class="mb-2">
             <a class="btn btn-secondary" href="{{ route('admin.informations.index') }}">戻る</a>
         </div>
-        <h3 class="rounded-top p-2 card-header text-success shadow-sm" style="background-color :#cdeee0">{{ $information->title }}</h3>
-        <div class="d-flex flex-column">
-            <div class="p-3">
-                <h4>本文</h4>
-                <div>
-                    <ul style="padding-left:0px !important;">
-                        <li class="list-unstyled">{{ $information->text }}</li>
-                    </ul>
-                </div>
-            </div>
-            <div class="d-flex flex-column">
-                <div class="mt-2 p-2">
-                    <div class="fw-bold fs-5">作成日時</div>
-                    <div>{{ $information->created_at }}</div>
-                </div>
-                <div class="mt-2 p-2">
-                    <div class="fw-bold fs-5">更新日時</div>
-                    <div>{{ $information->updated_at }}</div>
-                </div>
-                <div class="mt-2 p-2">
-                <div class="fw-bold fs-5">Action</div>
-                    <div class="action-btn">
-                        <a class="btn btn-success" href="{{ route('admin.informations.edit', ['information' => $information->id]) }}">編集</a>
-                        <form action="{{ route('admin.informations.destroy', $information) }}" method="post" class="d-inline">
-                            @csrf
-                            @method('delete')
-                            <input class="btn btn-danger" type="submit" value="削除"
-                            onClick="return confirm('本当に削除しますか？');">
-                        </form>
+        <div class="mt-3">
+            <h3 class="rounded-top p-2 card-header text-success shadow-sm" style="background-color: #cdeee0">{{ $information->title }}</h3>
+            <div class="border">
+                <div class="d-flex flex-column">
+                    <div class="mt-2 p-3">
+                        <h4>本文</h4>
+                        <div class="border">
+                            <div class="p-2">
+                                <ul style="padding-left: 0px !important;">
+                                    <li class="list-unstyled">{{ $information->text }}</li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                        <div class="mt-2 p-3">
+                            <div class="fw-bold fs-5">作成日時</div>
+                            <div>{{ $information->created_at }}</div>
+                        </div>
+                        <div class="mt-2 p-3">
+                            <div class="fw-bold fs-5">更新日時</div>
+                            <div>{{ $information->updated_at }}</div>
+                        </div>
+                        <div class="mt-2 p-3">
+                            <div class="fw-bold fs-5">Action</div>
+                            <div class="action-btn">
+                                <a class="btn btn-success" href="{{ route('admin.informations.edit', ['information' => $information->id]) }}">編集</a>
+                                <form action="{{ route('admin.informations.destroy', $information) }}" method="post" class="d-inline">
+                                    @csrf
+                                    @method('delete')
+                                    <input class="btn btn-danger" type="submit" value="削除" onClick="return confirm('本当に削除しますか？');">
+                                </form>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
         </div>
-    </div>
     {{-- footer --}}
     @include('footer')
 </body>
