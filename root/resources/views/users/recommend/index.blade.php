870bf1d4e6ea3efc7e089075c7ba35654a5fe560
Sora Sueshige <sora.sueshige@epkotsoftware.co.jp>
2024-01-26 11:33:02 +0900

    動画相性診断のレイアウト変更

--- root/resources/views/users/recommend/index.blade.php
+++ root/resources/views/users/recommend/index.blade.php
@@ -2,25 +2,33 @@
 <html lang="ja">
 <head>
     @include('head')
+    <link rel="stylesheet" href="/css/recommend.css">
     <title>動画相性診断</title>
 </head>
 
 <body>
 @include('users.header')
-<p>{{ $q['q_order']}}問目</p>
-<p>{{ $q['text'] }}</p>
+<div class="container my-4">
+    <div class="card text-bg-info" style="text-align: center;">
+        <div class="card-body shadow-sm">
+            <h3>{{ $q['q_order']}}問目</h3>
+            <p>{{ $q['text'] }}</p>
+        </div>
+    </div>
+    <div class="d-grid gap-2">
+        <form method="get" action="{{ route('users.select-courses.index') }}" class="answer">
+            <input type="hidden" value="{{ $q['q_id'] }}" name="q_id">
+            <input type="hidden" value="yes" name="answer">
+            <input type="submit" value="はい" class="btn btn-primary">
+        </form>
 
-<form method="get" action="{{ route('users.select-courses.index') }}">
-    <input type="hidden" value="{{ $q['q_id'] }}" name="q_id">
-    <input type="hidden" value="yes" name="answer">
-    <input type="submit" value="はい" class="btn btn-primary">
-</form>
-
-<form method="get" action="{{ route('users.select-courses.index') }}">
-    <input type="hidden" value="{{ $q['q_id'] }}" name="q_id">
-    <input type="hidden" value="no" name="answer">
-    <input type="submit" value="いいえ" class="btn btn-danger">
-</form>
+        <form method="get" action="{{ route('users.select-courses.index') }}" class="answer">
+            <input type="hidden" value="{{ $q['q_id'] }}" name="q_id">
+            <input type="hidden" value="no" name="answer">
+            <input type="submit" value="いいえ" class="btn btn-danger">
+        </form>
+    </div>
+</div>
 {{-- footer --}}
 @include('footer')
 </body>
