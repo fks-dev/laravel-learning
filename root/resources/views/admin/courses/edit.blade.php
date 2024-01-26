0194e731e498ae2ecfce6a9ee97d920beb71eefc
Yokouchi. A <147117744+akiko-yokouchi-epkotsoftware@users.noreply.github.com>
2024-01-26 17:12:15 +0900

    戻るボタンの追加

--- root/resources/views/admin/courses/edit.blade.php
+++ root/resources/views/admin/courses/edit.blade.php
@@ -7,7 +7,10 @@
 <body>
     @include('admin.header')
     <div class="mt-3 container">
-        <a href="{{ route('admin.courses.index') }}">&lt;&lt;戻る</a>
+        <div class="mb-2">
+            <a class="btn btn-secondary" href="{{ route('admin.courses.index') }}">戻る</a>
+        </div>
+
         <div class="border">
             <div class="p-2 bg-secondary text-white">コース編集</div>
 
