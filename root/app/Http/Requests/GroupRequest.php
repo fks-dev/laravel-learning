74f007f87aa16c2e3bf46dcdbbf266415a21df00
k.koya <koya.katsumata@epkotsoftware.co.jp>
2024-02-13 13:37:24 +0900

    GroupRequest.phpにバリデーションを追加

--- root/app/Http/Requests/GroupRequest.php
+++ root/app/Http/Requests/GroupRequest.php
@@ -22,7 +22,7 @@ class GroupRequest extends FormRequest
     public function rules(): array
     {
         return [
-            'group_name' => 'required',
+            'group_name' => 'required|string|max:255',
             'remarks'          => 'nullable|string|max:500'
         ];
     }
