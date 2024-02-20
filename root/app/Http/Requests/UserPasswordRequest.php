d6e44f896359f212858591faa5a56f554d70175b
BethFan <crashgetyou@outlook.jp>
2024-02-20 11:33:52 +0900

    属性名の変更と最大文字数の条件を追加

--- root/app/Http/Requests/UserPasswordRequest.php
+++ root/app/Http/Requests/UserPasswordRequest.php
@@ -23,8 +23,15 @@ class UserPasswordRequest extends FormRequest
     public function rules(): array
     {
         return [
-            'password' => 'required',
-            'new_password' => 'required|confirmed',
+            'password' => 'required|max:255',
+            'new_password' => 'required|confirmed|max:255',
+        ];
+    }
+
+    public function attributes()
+    {
+        return [
+            'password' => 'パスワード',
         ];
     }
 }
