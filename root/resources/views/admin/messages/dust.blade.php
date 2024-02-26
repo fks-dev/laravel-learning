e2db3b4c147a4f5a44566f0e30a24862baf57999
BethFan <crashgetyou@outlook.jp>
2024-02-26 14:23:58 +0900

    管理者メッセージの各viewファイルにパンくずリストを設定

--- root/resources/views/admin/messages/dust.blade.php
+++ root/resources/views/admin/messages/dust.blade.php
@@ -8,6 +8,13 @@
 <body>
 @include('admin.header')
 <div class="mt-5 container">
+    <nav style="--bs-breadcrumb-divider: '>';" aria-label="breadcrumb">
+        <ol class="breadcrumb">
+        <li class="breadcrumb-item"><a href="{{ route('admin.admin-management.index') }}">管理画面トップ</a></li>
+        <li class="breadcrumb-item"><a href="{{ route('admin.messages.index') }}">受信一覧</a></li>
+        <li class="breadcrumb-item active" aria-current="page">ゴミ箱一覧</li>
+        </ol>
+    </nav>
     <div class="d-flex justify-content-between">
         <h2 class="col">ゴミ箱</h2>
         <div class="col-auto">
--- root/resources/views/admin/messages/reply.blade.php
+++ root/resources/views/admin/messages/reply.blade.php
@@ -6,6 +6,14 @@
 </head>
 @include('admin.header')
 <div class="mt-3 container">
+    <nav style="--bs-breadcrumb-divider: '>';" aria-label="breadcrumb">
+        <ol class="breadcrumb">
+        <li class="breadcrumb-item"><a href="{{ route('admin.admin-management.index') }}">管理画面トップ</a></li>
+        <li class="breadcrumb-item"><a href="{{ route('admin.messages.index') }}">受信一覧</a></li>
+        <li class="breadcrumb-item"><a href="{{ route('admin.messages.show', $message) }}">メッセージ内容</a></li>
+        <li class="breadcrumb-item active" aria-current="page">返信</li>
+        </ol>
+    </nav>
         <div class="mb-2">
             <a class="btn btn-secondary" href="{{ route('admin.messages.show', $message) }}">戻る</a>
         </div>
