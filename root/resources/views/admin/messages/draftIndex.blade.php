8706c75da7f99abbf584b22baf8185922256ae2e
BethFan <crashgetyou@outlook.jp>
2024-02-26 11:34:17 +0900

    下書き一覧にパンくずリストを設定

--- root/resources/views/admin/messages/draftIndex.blade.php
+++ root/resources/views/admin/messages/draftIndex.blade.php
@@ -8,6 +8,13 @@
 <body>
 @include('admin.header')
 <div class="mt-5 container">
+    <nav style="--bs-breadcrumb-divider: '>';" aria-label="breadcrumb">
+        <ol class="breadcrumb">
+        <li class="breadcrumb-item"><a href="{{ route('admin.admin-management.index') }}">管理画面トップ</a></li>
+        <li class="breadcrumb-item"><a href="{{ route('admin.messages.index') }}">受信一覧</a></li>
+        <li class="breadcrumb-item active" aria-current="page">下書き一覧</li>
+        </ol>
+    </nav>
     <div class="d-flex justify-content-between">
         <h2 class="col">下書き一覧</h2>
         <div class="col-auto">
