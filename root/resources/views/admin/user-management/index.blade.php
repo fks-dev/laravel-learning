3dd3f247d49120ee5737661c3d6d71ea1ced04d4
kyotaTakiyama <kyota.takiyama@epkotsoftware.co.jp>
2024-02-29 15:28:23 +0900

    画面表示の不具合を修正

--- root/resources/views/admin/user-management/index.blade.php
+++ root/resources/views/admin/user-management/index.blade.php
@@ -10,13 +10,14 @@
 <body>
     @include('admin.header')
     <div class="mt-5 container">
-        @include('admin.menu')
         <nav style="--bs-breadcrumb-divider: '>';" aria-label="breadcrumb">
             <ol class="breadcrumb">
             <li class="breadcrumb-item"><a href="{{ route('admin.admin-management.index') }}">管理画面トップ</a></li>
             <li class="breadcrumb-item active" aria-current="page">ユーザー 一覧画面</li>
             </ol>
         </nav>
+
+        @include('admin.menu')
         <div class="d-flex justify-content-between">
             <h2 class="me-4">ユーザー 一覧</h2>
             <div class="me-auto">
