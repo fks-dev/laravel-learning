4e4be7e46cfaaefda28466c195950c36e8164951
NatsukiWatanabe-epkot <149548180+NatsukiWatanabe-epkot@users.noreply.github.com>
2024-03-06 16:26:16 +0900

    コンテンツ一覧画面のパンくずリスト修正 #353

--- root/resources/views/admin/contents/index.blade.php
+++ root/resources/views/admin/contents/index.blade.php
@@ -14,6 +14,7 @@
         <nav style="--bs-breadcrumb-divider: '>';" aria-label="breadcrumb">
             <ol class="breadcrumb">
               <li class="breadcrumb-item"><a href="{{ route('admin.admin-management.index') }}">管理画面トップ</a></li>
+              <li class="breadcrumb-item"><a href="{{ route('admin.courses.index') }}">コース一覧</a></li>
               <li class="breadcrumb-item active" aria-current="page">コース名『{{ $course->title }}』のコンテンツ一覧</li>
             </ol>
         </nav>
