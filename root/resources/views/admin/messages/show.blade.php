f34f862f56d9a0622e5addbf85adb2fcc8a7e973
BethFan <crashgetyou@outlook.jp>
2024-03-06 10:11:14 +0900

    パンくずリストの条件式とコンフリクトの修正

--- root/resources/views/admin/messages/show.blade.php
+++ root/resources/views/admin/messages/show.blade.php
@@ -13,11 +13,10 @@
             <li class="breadcrumb-item"><a href="{{ route('admin.messages.index') }}">受信一覧</a></li>
             @if ( $backRoute === route('admin.messages.sent'))
             <li class="breadcrumb-item"><a href="{{ $backRoute }}?page={{$currentPage}}">送信済み一覧</a></li>
-            @endif
-            @if($backRoute == route('admin.messages.dust'))
+            @elseif($backRoute == route('admin.messages.dust'))
             <li class="breadcrumb-item"><a href="{{ $backRoute }}?page={{$currentPage}}">ゴミ箱一覧</a></li>
             @endif
-            <li class="breadcrumb-item active" aria-current="page">メッセージ内容</li>
+            <li class="breadcrumb-item active" aria-current="page">メッセージ詳細</li>
             </ol>
         </nav>
         <div class="mb-2">
